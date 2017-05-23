6958006d13786d58e0c98847d92032f03a9218d5
Jan Kundrát <jan.kundrat@cesnet.cz>
2017-05-23 19:29:29 +0200

    Restart sysrepod, netopeer2-server and the cla-sysrepod upon failures
    In addition, a restart of sysrepod is bad enough so that it restarts
    everything else, and a failed netopeer2-server results in a restart of
    sysrepod (and therefore everything else) because there's quite some
    potential for confusion otherwise.
    
    I saw that confusion; it wasn't pretty.
    
    Change-Id: Iba18f594014c70e94727b69cd81ac9c57a0eafdb

--- board/czechlight/common/overlay/root/doit-CL-ROADM-v2.sh
+++ board/czechlight/common/overlay/root/doit-CL-ROADM-v2.sh
@@ -10,4 +10,10 @@ done
 sysrepoctl --install --search-dir /usr/share/cla-sysrepo/yang --yang /usr/share/cla-sysrepo/yang/czechlight-roadm-v2.yang
 sysrepocfg --datastore=startup --import=/usr/share/cla-sysrepo/yang/czechlight-roadm-v2.startup.xml czechlight-roadm-v2
 
+systemctl enable czechlight-roadm-v2
+systemctl start czechlight-roadm-v2
+
+systemctl enable netopeer2-server
+systemctl start netopeer2-server
+
 echo 'cla-sysrepod --properties-log-level=5 --sr-bridge-log-level=5 --sysrepo-log-level=3 --driver=CL-ROADMv2 --port=/dev/ttyUSB0' >> ~/.ash_history
