16091fa8fab4491f8799150b24194dce3abacf16
Jan Kundrát <jan.kundrat@cesnet.cz>
2017-10-02 18:27:15 +0200

    systemd: Use overlay for specifying watchdog parameters
    There's no reason to sed stuff. This way also makes it easier to support
    running with empty /etc.
    
    Change-Id: I16c0dba53682df574ee119951f47c7c6d4071d61

--- board/czechlight/common/systemd-watchdog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-sed -i -E "s/(#?)ShutdownWatchdogSec=(.*)/ShutdownWatchdogSec=60/" ${TARGET_DIR}/etc/systemd/system.conf
-sed -i -E "s/(#?)RuntimeWatchdogSec=(.*)/RuntimeWatchdogSec=60/" ${TARGET_DIR}/etc/systemd/system.conf
