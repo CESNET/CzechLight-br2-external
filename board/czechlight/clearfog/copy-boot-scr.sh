bdaf934360db88a5da23e23083c1179b53347cd5
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-01-12 18:18:35 +0100

    clearfog: Implement USB booting
    Here's how to load this from a possible bricked device:
    
    1) Set the boot switches to 01001 (yes, it's different from what the doc
    says)
    2) Load the bootloader via UART, e.g.:
      `./tools/kwboot -b ./u-boot-spl.kwb -t -p /dev/ttyUSB0`
    3) Interrupt U-Boot's autoboot to get a prompt
    4) Plug a USB flash with images/usb-flash.img loaded
    5) One on the U-Boot prompt, execute:
      `usb start; fatload usb 0:1 00800000 usb-boot.scr; source 00800000`
    6) Get a nice emergency shell
    
    Change-Id: Icef4badc9105bbed78ad949c060c0a46e9ff7f76

--- board/czechlight/clearfog/copy-boot-scr.sh
+++ board/czechlight/clearfog/copy-boot-scr.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 install -m 0644 -D $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+
+# Prepare a boot script for USB booting as well
+${HOST_DIR}/bin/mkimage -C none -A arm -T script \
+	-d ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/clearfog/usb-boot.scr.txt ${BINARIES_DIR}/usb-boot.scr
