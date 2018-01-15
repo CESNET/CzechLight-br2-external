a805002f94978b218427e4ee1c8617d5aba1f412
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-01-15 19:52:09 +0100

    clearfog: eMMC: Fix superblock-in-future issues
    Because the on-board RTC might be completely broken, it's quite possible
    that the times embedded in the FS might cause `fsck` to complain and
    therefore also systemd to get confused. Let's just run `fsck` for all
    partitions as a lame attempt at fixing this.
    
    Change-Id: I006e09944ef7fb4fb36e803cb15885bd0f0f2220

--- board/czechlight/clearfog/usb-reflash-factory.sh
+++ board/czechlight/clearfog/usb-reflash-factory.sh
@@ -6,6 +6,10 @@ mkdir /tmp/sdcard.image
 mount /mnt/sdcard.img.squashfs /tmp/sdcard.image/
 blkdiscard /dev/mmcblk0
 ddrescue --force /tmp/sdcard.image/sdcard.img /dev/mmcblk0
+fsck -y /dev/mmcblk0p1 || true
+fsck -y /dev/mmcblk0p2 || true
+fsck -y /dev/mmcblk0p3 || true
+fsck -y /dev/mmcblk0p4 || true
 echo 0 > /sys/block/mmcblk0boot0/force_ro
 ddrescue --force /mnt/u-boot-spl.kwb /dev/mmcblk0boot0
 echo 1 > /sys/block/mmcblk0boot0/force_ro
