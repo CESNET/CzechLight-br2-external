63a515b7b30ffc41a0a836ed5d1c7b08969bf6d8
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-01-16 14:28:27 +0100

    clearfog: Create valid squashfs images for USB flashing
    If the squashfs file gets reused from the last time, then the file gets
    renamed automagically. That's not what we want.
    
    Change-Id: Iaff83dc24306cc18e820f1e1b749be4c9e6768e8

--- board/czechlight/clearfog/usb-flash-genimage.sh
+++ board/czechlight/clearfog/usb-flash-genimage.sh
@@ -4,7 +4,7 @@ set -ex
 
 install -m 0644 -D ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/clearfog/usb-reflash-factory.sh ${BINARIES_DIR}/usb-reflash-factory.sh
 
-mksquashfs ${BINARIES_DIR}/sdcard.img ${BINARIES_DIR}/sdcard.img.squashfs -root-owned -comp gzip -progress
+mksquashfs ${BINARIES_DIR}/sdcard.img ${BINARIES_DIR}/sdcard.img.squashfs -root-owned -comp gzip -progress -noappend
 
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 rm -rf "${GENIMAGE_TMP}"
