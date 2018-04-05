ff516d115403f66d8c32655355add7e80573a4d3
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-04-05 21:32:53 +0200

    RAUC: turn this into a configurable target
    I am not using the generic rootfs target because it is optimized for
    using fakeroot, and because it extracts that tarball from rootfs-common.
    Rather than fighting the hooks (ROOTFS_XXX_POST_GEN_HOOKS is the only
    thing which runs outside of fakeroot) and doing extra work by unpacking,
    let's go all the way down to raw Makefile.
    
    Change-Id: I075204291fbeac8d1af97a0a7c1254054ef1f594

---
+++ package/czechlight-rauc/czechlight-rauc.mk
@@ -0,0 +1,33 @@
+CZECHLIGHT_RAUC_TMP_TARGET_DIR = $(FS_DIR)/rootfs.czechlight-rauc.tmp
+
+$(BINARIES_DIR)/update.raucb: host-rauc rootfs-tar
+	@$(call MESSAGE,"Generating RAUC update bundle")
+	$(RM) -rf $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
+	mkdir -p $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
+	sed -e 's|CZECHLIGHT_RAUC_VERSION|dev|' -e 's|CZECHLIGHT_RAUC_COMPATIBLE|$(call qstrip,$(CZECHLIGHT_RAUC_COMPATIBLE))|' \
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/board/czechlight/common/rauc-manifest.raucm.in \
+		> $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)/manifest.raucm
+
+	$(RM) -f $(BINARIES_DIR)/update.raucb
+	ln $(BINARIES_DIR)/rootfs.tar.xz $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
+	tar -cJf $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)/cfg.tar.xz -T /dev/null
+	cp $(BR2_EXTERNAL_CZECHLIGHT_PATH)/board/czechlight/common/rauc-hook.sh $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)/hook.sh
+
+	$(HOST_DIR)/usr/bin/rauc \
+		--cert $(BR2_EXTERNAL_CZECHLIGHT_PATH)/crypto/rauc-cert.pem \
+		--key $(BR2_EXTERNAL_CZECHLIGHT_PATH)/crypto/rauc-key.pem \
+		bundle $(CZECHLIGHT_RAUC_TMP_TARGET_DIR) $(BINARIES_DIR)/update.raucb
+
+rootfs-czechlight-rauc: $(BINARIES_DIR)/update.raucb
+
+rootfs-czechlight-rauc-show-depends:
+	@echo host-rauc rootfs-tar
+
+.PHONY: rootfs-czechlight-rauc rootfs-czechlight-rauc-show-depends
+
+ifeq ($(CZECHLIGHT_RAUC_ROOTFS),y)
+TARGETS_ROOTFS += rootfs-czechlight-rauc
+ifeq (x$(call qstrip,$(CZECHLIGHT_RAUC_COMPATIBLE)),x)
+$(error CZECHLIGHT_RAUC_COMPATIBLE cannot be empty)
+endif
+endif
