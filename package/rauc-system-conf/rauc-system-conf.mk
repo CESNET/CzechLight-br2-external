39163ced87687512bbaa4f0fb08c8ea774887133
Miroslav Mareš <mmares@cesnet.cz>
2018-04-05 21:09:10 +0200

    Created rauc-system-conf package for generating RAUC system.conf
    Change-Id: I3fd894775de4d9649eab30d754b666193fb41ff1

---
+++ package/rauc-system-conf/rauc-system-conf.mk
@@ -0,0 +1,24 @@
+RAUC_SYSTEM_CONF_INSTALL_TARGET = YES
+RAUC_SYSTEM_CONF_INSTALL_IMAGES = NO
+
+# Find out which bootloader should we write into system.conf:
+ifeq ($(BR2_TARGET_GRUB2), y)
+	CZECHLIGHT_RAUC_BOOTLOADER_NAME = grub
+else ifeq ($(BR2_TARGET_UBOOT), y)
+	CZECHLIGHT_RAUC_BOOTLOADER_NAME = uboot
+else
+	$(error Unsupported bootloader for RAUC)
+endif
+
+# Generate the /etc/rauc/system.conf file
+define RAUC_SYSTEM_CONF_INSTALL_TARGET_CMDS
+	$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/rauc-system-conf/generate-system-conf.sh > $(TARGET_DIR)/etc/rauc/system.conf
+	sed -i -E -e "s|CZECHLIGHT_RAUC_BOOTLOADER_NAME|$(CZECHLIGHT_RAUC_BOOTLOADER_NAME)|" \
+		-e "s|CZECHLIGHT_RAUC_SLOT_A_ROOTFS_DEV|$(call qstrip, $(CZECHLIGHT_RAUC_SLOT_A_ROOTFS_DEV))|" \
+		-e "s|CZECHLIGHT_RAUC_SLOT_A_CFG_DEV|$(call qstrip, $(CZECHLIGHT_RAUC_SLOT_A_CFG_DEV))|" \
+		-e "s|CZECHLIGHT_RAUC_SLOT_B_ROOTFS_DEV|$(call qstrip, $(CZECHLIGHT_RAUC_SLOT_B_ROOTFS_DEV))|" \
+		-e "s|CZECHLIGHT_RAUC_SLOT_B_CFG_DEV|$(call qstrip, $(CZECHLIGHT_RAUC_SLOT_B_CFG_DEV))|" \
+		$(TARGET_DIR)/etc/rauc/system.conf
+endef
+
+$(eval $(generic-package))
