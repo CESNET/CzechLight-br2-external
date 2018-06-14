0e14035d8d7c838a0a65656a157a8489a3a61e1a
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-06-14 16:22:12 +0200

    Start WDT pinging only once everything is up
    Previously, systemd started pinging the HW watchdog too early. It was
    possible to arrive at a system which is more or less FUBAR, yet the WS
    was happily being periodically reset by systemd's event loop.
    
    This change ensures that the watchdog resetting only begins once the
    rest of the system is stable. I've added a new target which brings in
    the usual multi-user.target, and actually depends on it. This means that
    once "everything" is up and running, we mark this slot as a correct one,
    and begin pinging the watchdog.
    
    The "running" timeout is now set to 30 seconds. This means that systemd
    will ping the HW once every 15s, and that a failure to do so for half a
    minute results in an immediate reboot. There's no shutdown timer because
    we don't really care about what happens once we started to shutdown. Our
    only desire is to reboot, eventually, and this one looks like it can do
    it :).
    
    Change-Id: Ib94992814705b4a6d4d712db9e443d6a784cec7f

--- package/czechlight-rauc/czechlight-rauc.mk
+++ package/czechlight-rauc/czechlight-rauc.mk
@@ -70,10 +70,13 @@ define CZECHLIGHT_RAUC_INSTALL_TARGET_CMDS
 		-e "s|CZECHLIGHT_RAUC_SLOT_B_ROOTFS_DEV|$(call qstrip,$(CZECHLIGHT_RAUC_SLOT_B_ROOTFS_DEV))|" \
 		-e "s|CZECHLIGHT_RAUC_SLOT_B_CFG_DEV|$(call qstrip,$(CZECHLIGHT_RAUC_SLOT_B_CFG_DEV))|" \
 		$(TARGET_DIR)/etc/rauc/system.conf
-	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
+	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-rauc/rauc-mark-good.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/
-	ln -sf ../rauc-mark-good.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-rauc/enable-hw-watchdog.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-rauc/czechlight.target \
+		$(TARGET_DIR)/usr/lib/systemd/system/
 endef
 
 $(eval $(generic-package))
