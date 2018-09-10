cc6169e1864aff8d40b6419ef7756e34bfa851d1
Jan Kundrát <kundrat@cesnet.cz>
2018-09-10 20:34:31 +0200

    clearfog: Unit files for the SDN ROADM
    Change-Id: I1df79eddd217bb757f4ad2e8e67ad7914d6b9aee

--- package/cla-sysrepo/cla-sysrepo.mk
+++ package/cla-sysrepo/cla-sysrepo.mk
@@ -6,4 +6,26 @@ CLA_SYSREPO_DEPENDENCIES = sysrepo docopt-cpp spdlog netsnmp systemd libgpiod
 CLA_SYSREPO_CONF_OPTS = -DTHREADS_PTHREAD_ARG:STRING=-pthread
 CLA_SYSREPO_LICENSE_FILES = LICENSE.md
 
+CLA_SYSREPO_ONE_MODEL = \
+	sed \
+		-e "s/__MODEL__/$1/g" \
+		-e "s/__YANG__/$2/g" \
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/cla-sysrepo/cla-install-yang.service.in \
+		> $(TARGET_DIR)/usr/lib/systemd/system/cla-install-yang-$1.service ; \
+	sed \
+		-e "s/__MODEL__/$1/g" \
+		-e "s/__YANG__/$2/g" \
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/cla-sysrepo/cla-appliance.service.in \
+		> $(TARGET_DIR)/usr/lib/systemd/system/cla-$1.service ; \
+	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants ; \
+	ln -sf ../cla-install-yang-$1.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/ ; \
+	ln -sf ../cla-$1.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/ ;
+
+define CLA_SYSREPO_INSTALL_INIT_SYSTEMD
+	$(call CLA_SYSREPO_ONE_MODEL,sdn-roadm-add-drop,czechlight-roadm-device)
+	$(call CLA_SYSREPO_ONE_MODEL,sdn-roadm-line,czechlight-roadm-device)
+endef
+
 $(eval $(cmake-package))
