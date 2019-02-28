c80d5cd3b0d6e482d3baa6728ef9459918c3b380
Jan Kundrát <kundrat@cesnet.cz>
2019-02-28 11:32:49 +0100

    CI coverage
    TODO:
    - once netconf-cli moves to shared dependencies. enable it again
    
    Change-Id: I81f2f3195aa1dbe4527135861cd486eb559d65ed

---
+++ ci/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eux -o pipefail
+shopt -s failglob
+
+ZUUL_JOB_NAME=$(jq < ~/zuul-env.json -r '.job')
+ZUUL_PROJECT_SRC_DIR=$HOME/$(jq < ~/zuul-env.json -r '.project.src_dir')
+ZUUL_PROJECT_SHORT_NAME=$(jq < ~/zuul-env.json -r '.project.short_name')
+
+BUILD_DIR=~/build
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+${ZUUL_PROJECT_SRC_DIR}/dev-setup-git.sh
+
+if [[ "${ZUUL_JOB_NAME}" =~ clearfog ]]; then
+    make czechlight_clearfog_defconfig
+else
+    echo "Unrecognized job name, cannot determine defconfig target"
+    exit 1
+fi
+
+echo BR2_PRIMARY_SITE=\"https://ci-logs.gerrit.cesnet.cz/t/public/mirror/buildroot/\" >> .config
+
+make
+mv images/update.raucb ~/zuul-output/artifacts/
+
+if [[ "${ZUUL_JOB_NAME}" =~ clearfog ]]; then
+    mv images/u-boot-spl.kwb ~/zuul-output/artifacts/
+fi
+
+# TODO: USB image as well? (`fallocate -d` to make it sparse)
+# TODO: make legal-info
