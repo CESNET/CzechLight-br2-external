6a0773d62577c82fad615e4fbc14b5412b1ae18a
Jan Kundrát <kundrat@cesnet.cz>
2019-02-28 10:22:20 +0100

    CI coverage
    Change-Id: I81f2f3195aa1dbe4527135861cd486eb559d65ed

---
+++ ci/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux -o pipefail
+shopt -s failglob
+
+ZUUL_JOB_NAME=$(jq < ~/zuul-env.json -r '.job')
+ZUUL_PROJECT_SRC_DIR=$HOME/$(jq < ~/zuul-env.json -r '.project.src_dir')
+ZUUL_PROJECT_SHORT_NAME=$(jq < ~/zuul-env.json -r '.project.short_name')
+
+BUILD_DIR=~/build
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+${ZUUL_PROJECT_SRC_DIR}/dev-setup-git.sh
+
+if [[ "${ZUUL_JOB_NAME}" =~ clearfog ]]; then
+    make czechlight_clearfog_defconfig
+    make
+    mv images/update.raucb images/u-boot-spl.kwb ~/zuul-output/artifacts/
+else
+    echo "Unrecognized job name, cannot determine defconfig target"
+    exit 1
+fi
+
+# TODO: USB image as well? (`fallocate -d` to make it sparse)
+# TODO: make legal-info
