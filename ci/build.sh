0f0c7af13487d583658bf1c78ecd00663f38a3fc
Jan Kundrát <kundrat@cesnet.cz>
2019-03-07 15:50:52 +0100

    Use an experimental top-level parallel build
    Change-Id: I6e7e44a371ed28d3303e921dadb18cb9f5b9a2d4

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,7 @@ shopt -s failglob
 ZUUL_JOB_NAME=$(jq < ~/zuul-env.json -r '.job')
 ZUUL_PROJECT_SRC_DIR=$HOME/$(jq < ~/zuul-env.json -r '.project.src_dir')
 ZUUL_PROJECT_SHORT_NAME=$(jq < ~/zuul-env.json -r '.project.short_name')
+CI_PARALLEL_JOBS=$(grep -c '^processor' /proc/cpuinfo)
 
 BUILD_DIR=~/build
 mkdir ${BUILD_DIR}
@@ -23,7 +24,11 @@ fi
 echo BR2_PRIMARY_SITE=\"https://ci-logs.gerrit.cesnet.cz/t/public/mirror/buildroot\" >> .config
 make source
 
-make
+# host-python fails in parallel build for me with an error in the _hashlib extension
+make host-python
+# the rest can be built in parallel
+sed -i 's/^\(\.NOTPARALLEL\).*/#\1/' ${HOME}/${ZUUL_PROJECT_SRC_DIR}/submodules/buildroot/Makefile
+make -j${CI_PARALLEL_JOBS}
 mv images/update.raucb ~/zuul-output/artifacts/
 
 if [[ "${ZUUL_JOB_NAME}" =~ clearfog ]]; then
