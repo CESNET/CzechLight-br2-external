51c34e1f76d80a394d86bb94a7f0e81dafa9224a
Jan Kundrát <kundrat@cesnet.cz>
2019-04-04 21:00:19 +0200

    Coherent Add/Drop
    This device has an Oplink/Molex ITMA instead of the Lumentum OCM, but
    that does not need any DT configuration. There's also a second TLC59116
    for per-add-port status control.
    
    LED numbering on the add-on board goes backwards, but that's not really
    an issue.
    
    Change-Id: I285b730c16934d2638c005329253b0207fa69fde

--- package/czechlight-clearfog-leds/clearfog-test-leds.sh
+++ package/czechlight-clearfog-leds/clearfog-test-leds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LEDS=(NONE status:{red,green,blue} uid:{red,green,blue} line:{red,green,blue} sfp:{red,green,blue})
+LEDS=(NONE led5:{red,green,blue} status:{red,green,blue} uid:{red,green,blue} line:{red,green,blue} sfp:{red,green,blue} port1:{red,green} port2:{red,green} port3:{red,green} port4:{red,green} port5:{red,green} port6:{red,green} port7:{red,green} port8:{red,green})
 
 LAST=${#LEDS[@]}
 let "LAST2 = $LAST - 1"
