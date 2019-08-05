5fb4992a66940253ee4d4d88cd0398e58a75458b
Jan Kundrát <kundrat@cesnet.cz>
2019-08-05 18:27:25 +0200

    Refresh the BeagleBone Black image
    Barebox won't build (it complains about <linux/list.h> not found and I
    cannot easily solve that), so let's go back to U-Boot.
    
    The 2019.01 U-Boot release is buggy in its recognizing of partition
    tables (`fatls` complains, for example), and the whole thing reverts to
    a "legacy boot", so let's use the latest release, the 2019.07.
    
    I'm also giving up on the g_cdc module because the board that I'm using
    has a microUSB header for the debug console.
    
    - FIXME: Properly distinguish between an SD-card boot and an eMMC boot
    (right now it is hardcoded to boot from the SD card)
    - FIXME: test Rauc updates once eMMC works
    - FIXME: activate watchdog from U-Boot
    
    Change-Id: I013f145cf2fe24e41ea0dafd225e3feb1854712c

---
+++ board/czechlight/beaglebone/create-uEnv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+install -m 0644 -D $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+
+# prepare the static script with RAUC logic
+${HOST_DIR}/bin/mkimage -C none -A arm -T script \
+        -d ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/rauc.scr.txt ${BINARIES_DIR}/rauc.scr
+
+# ...and a simplified one for SD-card booting (no image determination)
+${HOST_DIR}/bin/mkimage -C none -A arm -T script \
+        -d ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/sd.scr.txt ${BINARIES_DIR}/sd.scr
