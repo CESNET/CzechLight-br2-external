0ed892b1ff476cc0072983280292e2a91e6bb5d2
Jan Kundrát <kundrat@cesnet.cz>
2019-08-07 14:11:15 +0200

    BeagleBone Black: let the default FW "boot from the SD card"
    I just don't want to hold that button at each power-up, and I have not
    added support for flashing to eMMC yet. The default, preinstalled
    bootlaoder tries to read uEnv.txt from the SDD card's boot partition and
    execute `uenvcmd` from there, so let's make it happy and adapt
    ourselves.
    
    The default U-Boot installed on-device (2015.01-00001-gb2412df) doesn't
    define ${scriptaddr}, so we have to provide it if we want our launchers
    to succeed.
    
    Change-Id: I50dc5177f4dd16435d4f5d17908db938e4cc5c3c

--- board/czechlight/beaglebone/create-uEnv.sh
+++ board/czechlight/beaglebone/create-uEnv.sh
@@ -2,6 +2,8 @@
 
 install -m 0644 -D $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
 
+cp ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/sd-uEnv.txt ${BINARIES_DIR}/
+
 # prepare the static script with RAUC logic
 ${HOST_DIR}/bin/mkimage -C none -A arm -T script \
         -d ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/rauc.scr.txt ${BINARIES_DIR}/rauc.scr
