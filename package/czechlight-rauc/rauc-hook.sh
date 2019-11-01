68f98ad07546f49baa15a082413c2d2595bc6091
Jan Kundrát <kundrat@cesnet.cz>
2019-11-01 20:18:04 +0100

    refactoring: make /cfg updates easily extensible
    Change-Id: Ib025d0efc9c522d8a26588180c98df89148e55f8

--- package/czechlight-rauc/rauc-hook.sh
+++ package/czechlight-rauc/rauc-hook.sh
@@ -4,9 +4,14 @@ case "$1" in
   slot-post-install)
     case "$RAUC_SLOT_CLASS" in
       cfg)
-        for DIR in etc random-seed ssh-user-auth; do
-          if [[ -d /cfg/$DIR ]]; then
-            cp -a /cfg/$DIR ${RAUC_SLOT_MOUNT_POINT}/
+        # whitelist so that we don't copy cruft or lost+found
+        for ITEM in \
+            etc \
+            random-seed \
+            ssh-user-auth \
+            ; do
+          if [[ -d /cfg/${ITEM} ]]; then
+            cp -a /cfg/${ITEM} ${RAUC_SLOT_MOUNT_POINT}/
           fi
         done
         ;;
