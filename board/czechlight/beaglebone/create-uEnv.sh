cb10e82abb1552d444603fef318405325f03d6ef
Jan Kundrát <kundrat@cesnet.cz>
2020-01-29 19:05:30 +0100

    bbb: use RAUC eMMC layout
    This, unfortunately, means that the system will no longer boot from the
    SD card:
    
    - U-Boot is set to store its env in the p1 VFAT partition
    - rootfs' /boot is something else (kernel, etc) than what gets mounted
    on system's /boot
    - rootfs' /boot/boot.scr contains device numbers which refer to the eMMC
    block device, not to the SD card
    
    I think it might be possible to add some workarounds, such as exporting
    the number of an "active MMC device" to an U-Boot variable or something,
    but hey, let's give up. The only thing that I need is *some* method of
    flashing content to the device, and any distro is good enough for that.
    
    Change-Id: Ia966448a05ed5ce52747f5e7169136232f5502c2

--- board/czechlight/beaglebone/create-uEnv.sh
+++ board/czechlight/beaglebone/create-uEnv.sh
@@ -2,12 +2,6 @@
 
 install -m 0644 -D $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
 
-cp ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/sd-uEnv.txt ${BINARIES_DIR}/
-
 # prepare the static script with RAUC logic
 ${HOST_DIR}/bin/mkimage -C none -A arm -T script \
         -d ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/rauc.scr.txt ${BINARIES_DIR}/rauc.scr
-
-# ...and a simplified one for SD-card booting (no image determination)
-${HOST_DIR}/bin/mkimage -C none -A arm -T script \
-        -d ${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/beaglebone/sd.scr.txt ${BINARIES_DIR}/sd.scr
