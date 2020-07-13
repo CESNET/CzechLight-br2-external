d2897bab4a27e61ff02197dfb343bff79db4e8a5
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-07-13 16:49:28 +0200

    Build velia daemon
    Introduce package for velia, configure build.
    
    Change-Id: I84d724918a3a6c7654eb113abce4d7068d7770a6
    Co-authored-by: Jan Kundrát <kundrat@cesnet.cz>
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/velia/+/2822
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/velia/+/2822
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/velia/+/2822

--- package/czechlight-clearfog-leds/czechlight-clearfog-leds.mk
+++ package/czechlight-clearfog-leds/czechlight-clearfog-leds.mk
@@ -1,19 +1,9 @@
-ifdef CZECHLIGHT_CLEARFOG_LEDS_BOOT
-define CZECHLIGHT_CLEARFOG_LEDS_BOOT_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 0644 \
-                $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-clearfog-leds/czechlight-clearfog-leds-boot.service \
-                $(TARGET_DIR)/usr/lib/systemd/system/
-        ln -sf ../czechlight-clearfog-leds-boot.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
-endef
-endif
-
 define CZECHLIGHT_CLEARFOG_LEDS_INSTALL_TARGET_CMDS
         mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
         $(INSTALL) -D -m 0644 \
                 $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-clearfog-leds/czechlight-clearfog-leds.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/
         ln -sf ../czechlight-clearfog-leds.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
-	$(CZECHLIGHT_CLEARFOG_LEDS_BOOT_INSTALL_TARGET_CMDS)
 	cp \
 		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-clearfog-leds/clearfog-test-leds.sh \
 		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-clearfog-leds/init-leds-edfa.sh \
