7e50de057babbf506fff9724b5c0f8aa7b39be40
Jan Kundrát <kundrat@cesnet.cz>
2020-10-26 16:06:16 +0100

    clearfog: make sure that journald logs can fit into /run
    systemd started using just 10% of RAM size for /run (and then bumped
    that to 20%, which is still a smaller amount than our log size default
    allowance of 200MB).
    
    And instead of putting three commands into KConfig, use a script for
    that.
    
    Fixes: b7181a3 (improve logging)
    Change-Id: I952ce37e9148f7389d3fff8a469352abf9c1127a

---
+++ board/czechlight/clearfog/post_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/clearfog/copy-boot-scr.sh
+
+${BR2_EXTERNAL_CZECHLIGHT_PATH}/board/czechlight/common/os-release.sh
+
+# enable about 250MB of ramfs for the journal log
+sed -i -n \
+	-e '/\s\/run\s/!p' \
+	-e '$a tmpfs /run tmpfs rw,nosuid,nodev,size=256000k,nr_inodes=819200,mode=755' \
+	${TARGET_DIR}/etc/fstab
