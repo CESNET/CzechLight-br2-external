9c73dea096dd14fbc53eb42edf0654ec5aa96164
Jan Kundrát <kundrat@cesnet.cz>
2020-11-02 22:10:16 +0100

    Use new, daemon-less sysrepo for YANG management
    Updating all dependencies so that we're using the new sysrepo. The easiest way
    there was via bringing in the new buildroot as well, which meant:
    
    - new RAUC, hence no patch needed anymore,
    - newer systemd, hence an updated patch,
    - New spdlog and fmt, which means that the version we ship in
      submodules/dependencies no longer works. Fix that by just relying on
      systemwide spdlog.
    - uboot-tools needed a patch because there's no /var/lock,
    - OpenSSH was previously started autmagically, now we're explicitly
      adding that to the list of packages (I really like SSH, don't you?).
    
    New sysrepo means that there's no `sysrepod` anymore, which required
    changing a bunch of other units so that they are not marked as
    `PartOf=sysrepod.service` anymore. Also, the command-line options for
    `sysrepocfg` and `sysrepoctl` changed.
    
    I had to patch buildroot so that it doesn't create SSH keys for the
    NETCONF server at build time, and that all required modules are actually
    installed. It turns out that new sysrepo doesn't really install
    anything, and the fun only starts with Netopeer2. Their install scripts
    a wee bit picky, but hey, we have bash, we can make it work.
    
    On the other hand, installation of *our* YANG modules is a bit hairy.
    There's just too much parallel activities going on, and therefore I have
    no idea how robust our callout to `sysrepoctl --apply` really is. As a
    cherry on top, `sysrepocfg` is particularly inconsistent when it comes
    to how the **** one is supposed to provision the initial config, etc
    etc. In the end, I ended up with a big hammer.
    
    Installation of all non-netopeer2 YANG modules now happens from just a
    single script, the previous way was very fragile and failed quite often
    -- probably because that "wonderful" new way of installing a module and
    possibly enabling a feature and also importing some data which might or
    might not be the required initial data, so *THAT* thing, depended on
    whether there already was or was not another SW connection. Wonderful.
    If only there was a oneliner for that, then we could have avoided all
    this stupid boilerplate. Oh well.
    
    On the other hand, the `cla-sysrepod` is now properly signalling its
    "up-and-running" status to systemd, *and* this up-and-running state is
    only reached once the configuration has been propagated to the optical
    HW. As a result, the HW watchdog will recover from an upload of a broken
    FW (or, alternatively, it will keep rebooting if the optical HW is
    FUBAR -- pick your poison, sir).
    
    Change-Id: I4b65a8fb345bfe7907a331d0de16294af1c36a78

---
+++ package/cla-sysrepo/czechlight-install-yang.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -ex
+
+IETF_HW_STATE=0
+YANG_ROADM=0
+YANG_COHERENT=0
+YANG_INLINE=0
+YANG_CALIBRATION=0
+
+YANG_DIR=/usr/share/cla-sysrepo/yang
+REPO=/etc/sysrepo/yang
+
+for ARG in $(cat /proc/cmdline); do
+    case "${ARG}" in
+        czechlight=*)
+            CZECHLIGHT="${ARG:11}"
+            ;;
+    esac
+done
+
+case "${CZECHLIGHT}" in
+    sdn-roadm-line)
+        YANG_ROADM=1
+        WITH_FEATURE=hw-line-9
+        IETF_HW_STATE=1
+        ;;
+    sdn-roadm-add-drop)
+        YANG_ROADM=1
+        WITH_FEATURE=hw-add-drop-20
+        IETF_HW_STATE=1
+        ;;
+    sdn-roadm-coherent-a-d)
+        IETF_HW_STATE=1
+        YANG_COHERENT=1
+        ;;
+    sdn-inline)
+        IETF_HW_STATE=1
+        YANG_INLINE=1
+        ;;
+    calibration-box)
+        YANG_CALIBRATION=1
+        ;;
+esac
+
+if [[ ${IETF_HW_STATE} == 1 && ! -f ${REPO}/ietf-hardware-state@2018-03-13.yang ]]; then
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/iana-hardware@2018-03-13.yang
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/ietf-hardware-state@2018-03-13.yang
+    sysrepoctl --change ietf-hardware-state --permissions 0664 --enable-feature hardware-sensor --apply
+fi
+
+if [[ ${YANG_ROADM} == 1 && ! -f ${REPO}/czechlight-roadm-device@2019-09-30.yang ]]; then
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-roadm-device.yang
+    sysrepoctl --change czechlight-roadm-device --group optics --permissions 0664 --apply
+    if [[ ${WITH_FEATURE} ]]; then
+        sysrepoctl --change czechlight-roadm-device --enable-feature ${WITH_FEATURE}
+    fi
+    sysrepocfg --datastore=startup --format=json --module=czechlight-roadm-device --import="${YANG_DIR}/${CZECHLIGHT}.json"
+fi
+
+if [[ ${YANG_COHERENT} == 1 && ! -f ${REPO}/czechlight-coherent-add-drop@2019-09-30.yang ]]; then
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-coherent-add-drop.yang
+    sysrepoctl --change czechlight-coherent-add-drop --group optics --permissions 0664 --apply
+    sysrepocfg --datastore=startup --format=json --module=czechlight-coherent-add-drop --new-data="${YANG_DIR}/${CZECHLIGHT}.json"
+fi
+
+if [[ ${YANG_INLINE} == 1 && ! -f ${REPO}/czechlight-inline-amp@2019-09-30.yang ]]; then
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-inline-amp.yang
+    sysrepoctl --change czechlight-inline-amp --group optics --permissions 0664 --apply
+    sysrepocfg --datastore=startup --format=json --module=czechlight-inline-amp --import="${YANG_DIR}/${CZECHLIGHT}.json"
+fi
+
+if [[ ${YANG_CALIBRATION} == 1 && ! -f ${REPO}/czechlight-calibration-device@2019-06-25.yang ]]; then
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-calibration-device.yang
+    sysrepoctl --change czechlight-calibration-device --group optics --permissions 0664 --apply
+    sysrepocfg --datastore=startup --format=json --module=czechlight-calibration-device --import="${YANG_DIR}/${CZECHLIGHT}.json"
+fi
+
+if [[ ! -f ${REPO}/czechlight-lldp@2020-08-25.yang ]]; then
+    sysrepoctl --search-dirs /usr/share/lldp-systemd-networkd-sysrepo/yang --install /usr/share/lldp-systemd-networkd-sysrepo/yang/czechlight-lldp.yang
+    sysrepoctl --change czechlight-lldp --permissions 0664 --apply
+fi
+
+mkdir -p /cfg/etc/sysrepo/
+cp -a /etc/sysrepo/data /etc/sysrepo/yang /cfg/etc/sysrepo/
