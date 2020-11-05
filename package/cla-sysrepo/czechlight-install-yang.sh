cf5dd8a4b3c7ff2cd95cea0c829a7ac0814c117a
Jan Kundrát <kundrat@cesnet.cz>
2020-11-05 15:59:11 +0100

    Sync CzechLight packages
    One YANG model got bumped, so I took this opportunity to clean up the
    rest as well.
    
    Also syncing everything while I'm at this.
    
    Change-Id: Ia7f69206e3e99f771426c094b2964f014b5c2f68
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/cla-sysrepo/+/3202
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/cla-sysrepo/+/3202
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/cla-sysrepo/+/3202
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/lldp-systemd-networkd-sysrepo/+/3201
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/lldp-systemd-networkd-sysrepo/+/3201
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/lldp-systemd-networkd-sysrepo/+/3201
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/netconf-cli/+/3204
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/netconf-cli/+/3204
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/netconf-cli/+/3204

--- package/cla-sysrepo/czechlight-install-yang.sh
+++ package/cla-sysrepo/czechlight-install-yang.sh
@@ -50,7 +50,7 @@ if [[ ${IETF_HW_STATE} == 1 && ! -f ${REPO}/ietf-hardware-state@2018-03-13.yang 
 fi
 
 if [[ ${YANG_ROADM} == 1 && ! -f ${REPO}/czechlight-roadm-device@2019-09-30.yang ]]; then
-    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-roadm-device.yang
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-roadm-device@2019-09-30.yang
     sysrepoctl --change czechlight-roadm-device --group optics --permissions 0664 --apply
     if [[ ${WITH_FEATURE} ]]; then
         sysrepoctl --change czechlight-roadm-device --enable-feature ${WITH_FEATURE}
@@ -59,25 +59,29 @@ if [[ ${YANG_ROADM} == 1 && ! -f ${REPO}/czechlight-roadm-device@2019-09-30.yang
 fi
 
 if [[ ${YANG_COHERENT} == 1 && ! -f ${REPO}/czechlight-coherent-add-drop@2019-09-30.yang ]]; then
-    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-coherent-add-drop.yang
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-coherent-add-drop@2019-09-30.yang
     sysrepoctl --change czechlight-coherent-add-drop --group optics --permissions 0664 --apply
     sysrepocfg --datastore=startup --format=json --module=czechlight-coherent-add-drop --new-data="${YANG_DIR}/${CZECHLIGHT}.json"
 fi
 
 if [[ ${YANG_INLINE} == 1 && ! -f ${REPO}/czechlight-inline-amp@2019-09-30.yang ]]; then
-    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-inline-amp.yang
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-inline-amp@2019-09-30.yang
     sysrepoctl --change czechlight-inline-amp --group optics --permissions 0664 --apply
     sysrepocfg --datastore=startup --format=json --module=czechlight-inline-amp --import="${YANG_DIR}/${CZECHLIGHT}.json"
 fi
 
 if [[ ${YANG_CALIBRATION} == 1 && ! -f ${REPO}/czechlight-calibration-device@2019-06-25.yang ]]; then
-    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-calibration-device.yang
+    sysrepoctl --search-dirs ${YANG_DIR} --install ${YANG_DIR}/czechlight-calibration-device@2019-06-25.yang
     sysrepoctl --change czechlight-calibration-device --group optics --permissions 0664 --apply
     sysrepocfg --datastore=startup --format=json --module=czechlight-calibration-device --import="${YANG_DIR}/${CZECHLIGHT}.json"
 fi
 
-if [[ ! -f ${REPO}/czechlight-lldp@2020-08-25.yang ]]; then
-    sysrepoctl --search-dirs /usr/share/lldp-systemd-networkd-sysrepo/yang --install /usr/share/lldp-systemd-networkd-sysrepo/yang/czechlight-lldp.yang
+if [[ ! -f ${REPO}/czechlight-lldp@2020-11-04.yang ]]; then
+    if compgen -G "${REPO}/czechlight-lldp@*.yang" >/dev/null; then
+        sysrepoctl --search-dirs /usr/share/lldp-systemd-networkd-sysrepo/yang --update /usr/share/lldp-systemd-networkd-sysrepo/yang/czechlight-lldp@2020-11-04.yang
+    else
+        sysrepoctl --search-dirs /usr/share/lldp-systemd-networkd-sysrepo/yang --install /usr/share/lldp-systemd-networkd-sysrepo/yang/czechlight-lldp@2020-11-04.yang
+    fi
     sysrepoctl --change czechlight-lldp --permissions 0664 --apply
 fi
 
