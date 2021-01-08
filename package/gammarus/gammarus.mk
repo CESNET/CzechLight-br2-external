fe0c1ee8d9ccb45c58fdac5f35e9b8cd8c66970a
Jan Kundrát <kundrat@cesnet.cz>
2021-01-08 21:10:07 +0100

    Sync all packages
    Change-Id: Ib2060ea850d1121d5025183b6c5b17d8962f435b
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/gammarus/+/3367
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/gammarus/+/3367

--- package/gammarus/gammarus.mk
+++ package/gammarus/gammarus.mk
@@ -9,4 +9,8 @@ define GAMMARUS_INSTALL_TARGET_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
 endef
 
+define GAMMARUS_USERS
+	yangnobody 333666 nobody * - - - Anonymous operations via RESTCONF
+endef
+
 $(eval $(generic-package))
