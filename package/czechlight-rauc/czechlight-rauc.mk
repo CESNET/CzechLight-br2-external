CESNET/CzechLight-br2-external

commit ca178a4b5b613d2ee62d631dadbbd6b8667a81ed

RAUC: mark a modified git checkout appropriately in image metadata
...so that it's visible even in `rauc status --detailed`.

Change-Id: Idb73d68f59cc53a89fb22b33836de837e7ff47f4

package/czechlight-rauc/czechlight-rauc.mk
@@ -5,7 +5,7 @@ $(BINARIES_DIR)/update.raucb: host-rauc rootfs-tar
 	$(RM) -rf $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
 	mkdir -p $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
 	sed \
-		-e 's|CZECHLIGHT_RAUC_IMAGE_VERSION|$(call qstrip,$(shell git --git-dir=$(BR2_EXTERNAL_CZECHLIGHT_PATH)/.git describe))|' \
+		-e 's|CZECHLIGHT_RAUC_IMAGE_VERSION|$(call qstrip,$(shell git --git-dir=$(BR2_EXTERNAL_CZECHLIGHT_PATH)/.git describe --dirty))|' \
 		-e 's|CZECHLIGHT_RAUC_COMPATIBLE|$(call qstrip,$(CZECHLIGHT_RAUC_COMPATIBLE))|' \
 		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-rauc/rauc-manifest.raucm.in \
 		> $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)/manifest.raucm

commit 9cea9b61bc18d5f0cf51449cea31f836b1a2163d

RAUC: do not mark every build as -dirty
These per-package steps are (obviously) not being run with the external
tree as their working directory, which means that git would always
consider the working tree as "modified" -- hence the dirty status.

Fix this by informing git what I wanted to do in the first place.

Fixes: ca178a4b5b613d2ee62d631dadbbd6b8667a81ed ("RAUC: mark a modified git checkout appropriately in image metadata")
Change-Id: I5cec209d8446c950f0f59ebaf50e39ce0ef9fedf

package/czechlight-rauc/czechlight-rauc.mk
@@ -5,7 +5,7 @@ $(BINARIES_DIR)/update.raucb: host-rauc rootfs-tar
 	$(RM) -rf $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
 	mkdir -p $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)
 	sed \
-		-e 's|CZECHLIGHT_RAUC_IMAGE_VERSION|$(call qstrip,$(shell git --git-dir=$(BR2_EXTERNAL_CZECHLIGHT_PATH)/.git describe --dirty))|' \
+		-e 's|CZECHLIGHT_RAUC_IMAGE_VERSION|$(call qstrip,$(shell git --git-dir=$(BR2_EXTERNAL_CZECHLIGHT_PATH)/.git --work-tree=$(BR2_EXTERNAL_CZECHLIGHT_PATH) describe --dirty))|' \
 		-e 's|CZECHLIGHT_RAUC_COMPATIBLE|$(call qstrip,$(CZECHLIGHT_RAUC_COMPATIBLE))|' \
 		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-rauc/rauc-manifest.raucm.in \
 		> $(CZECHLIGHT_RAUC_TMP_TARGET_DIR)/manifest.raucm