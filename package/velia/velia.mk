9e77d05d5c8db97f977ddbe9cb81ad61f1d8cd11
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2021-01-27 14:45:57 +0100

    Update velia
    Due to recent changes in velia, where we split the daemon into several
    smaller ones, this commit adds service files for those new daemons
    (veliad-{health,hardware,system}) and also installs a new YANG model
    that is needed for the veliad-system functionality.
    
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/velia/+/3549
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/velia/+/3549
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/velia/+/3549
    Change-Id: I5aae8da2d7af95ead80070ad4cd18d7143e0f736

--- package/velia/velia.mk
+++ package/velia/velia.mk
@@ -7,16 +7,22 @@ VELIA_CONF_OPTS = -DTHREADS_PTHREAD_ARG:STRING=-pthread
 VELIA_LICENSE = Apache-2.0
 VELIA_LICENSE_FILES = LICENSE.md
 
+define VELIA_PREPARE_SERVICE
+    $(INSTALL) -D -m 0644 \
+            $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/velia/$1.service \
+            $(TARGET_DIR)/usr/lib/systemd/system/
+    ln -sf ../$1.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
+endef
+
 define VELIA_INSTALL_INIT_SYSTEMD
         mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
         $(INSTALL) -D -m 0644 \
-                $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/velia/velia.service \
-                $(TARGET_DIR)/usr/lib/systemd/system/
-        ln -sf ../velia.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/
-
-		$(INSTALL) -D -m 0644 \
                 $(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/velia/max_match_rules.conf \
                 $(TARGET_DIR)/usr/share/dbus-1/system.d/
+
+        $(call VELIA_PREPARE_SERVICE,velia-health)
+        $(call VELIA_PREPARE_SERVICE,velia-hardware)
+        $(call VELIA_PREPARE_SERVICE,velia-system)
 endef
 
 $(eval $(cmake-package))
