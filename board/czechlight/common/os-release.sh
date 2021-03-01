18fb83e8939c8d33306448308106a025f3a0d39f
Jan Kundrát <kundrat@cesnet.cz>
2021-03-01 22:35:50 +0100

    velia version was not in /etc/os-release
    Also use the same order in both blocks while I'm at this.
    
    Change-Id: Id480dd894ae773b49804db12db2b68c580952696

--- board/czechlight/common/os-release.sh
+++ board/czechlight/common/os-release.sh
@@ -11,8 +11,9 @@ function czechlight_query_local_make_var {
 CLA_BR2_EXTERNAL_REV=$(czechlight_describe_git ${BR2_EXTERNAL_CZECHLIGHT_PATH})
 CLA_SYSREPO_REV=$(czechlight_describe_git $(czechlight_query_local_make_var CLA_SYSREPO_OVERRIDE_SRCDIR))
 NETCONF_CLI_REV=$(czechlight_describe_git $(czechlight_query_local_make_var NETCONF_CLI_OVERRIDE_SRCDIR))
-GAMMARUS_REV=$(czechlight_describe_git $(czechlight_query_local_make_var GAMMARUS_OVERRIDE_SRCDIR))
 CLA_CPP_DEPENDENCIES_REV=$(czechlight_describe_git $(czechlight_query_local_make_var CLA_SYSREPO_OVERRIDE_SRCDIR)/submodules/dependencies)
+GAMMARUS_REV=$(czechlight_describe_git $(czechlight_query_local_make_var GAMMARUS_OVERRIDE_SRCDIR))
+VELIA_REV=$(czechlight_describe_git $(czechlight_query_local_make_var VELIA_OVERRIDE_SRCDIR))
 
 sed -i \
 	-e 's/^VERSION_ID=/BUILDROOT_VERSION_ID=/' \
@@ -32,4 +33,5 @@ CLA_SYSREPO_VERSION=${CLA_SYSREPO_REV}
 NETCONF_CLI_VERSION=${NETCONF_CLI_REV}
 CPP_DEPENDENCIES_VERSION=${CLA_CPP_DEPENDENCIES_REV}
 GAMMARUS_VERSION=${GAMMARUS_REV}
+VELIA_VERSION=${VELIA_REV}
 EOF
