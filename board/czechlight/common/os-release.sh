CESNET/CzechLight-br2-external

commit 69c92de9356f18fd4e29355a67a1a90063676435

Fix git version reporting of `CzechLight/dependencies`
Fixes: 32ca34f Stop pinning `CzechLight/dependencies` via `cla-sysrepo` and `netconf-cli`
Change-Id: Ida23ba73a3639b2fd1add6336bffbdf3aedfb7b8

board/czechlight/common/os-release.sh
@@ -11,7 +11,7 @@ function czechlight_query_local_make_var {
 CLA_BR2_EXTERNAL_REV=$(czechlight_describe_git ${BR2_EXTERNAL_CZECHLIGHT_PATH})
 CLA_SYSREPO_REV=$(czechlight_describe_git $(czechlight_query_local_make_var CLA_SYSREPO_OVERRIDE_SRCDIR))
 NETCONF_CLI_REV=$(czechlight_describe_git $(czechlight_query_local_make_var NETCONF_CLI_OVERRIDE_SRCDIR))
-CLA_CPP_DEPENDENCIES_REV=$(czechlight_describe_git $(czechlight_query_local_make_var CLA_SYSREPO_OVERRIDE_SRCDIR)/submodules/dependencies)
+CLA_CPP_DEPENDENCIES_REV=$(czechlight_describe_git ${BR2_EXTERNAL_CZECHLIGHT_PATH}/submodules/dependencies)
 GAMMARUS_REV=$(czechlight_describe_git $(czechlight_query_local_make_var GAMMARUS_OVERRIDE_SRCDIR))
 VELIA_REV=$(czechlight_describe_git $(czechlight_query_local_make_var VELIA_OVERRIDE_SRCDIR))
 ROUSETTE_REV=$(czechlight_describe_git $(czechlight_query_local_make_var ROUSETTE_OVERRIDE_SRCDIR))

commit a5fd02c34d9d7c7d65062e82164c8d8b93cae99c

Use a correct version of the CzechLight/dependencies repo
Now that these are no longer pinned via cla-sysrepo or netconf-cli, it's
possible to support taking a specific commit in two different ways:

- as a git submodule, i.e., whatever is in the repo, ignoring Zuul,
- or from Zuul's job dependencies, overriding whatever is pinned via the git submodule

In a way, this achieves a similar end result with what we're currently
doing for `velia`, `cla-sysrepo`, `netconf-cli` and `rousette` -- except
these are BR-level packages which map 1:1 to a git submodule, whereas
the `CzechLight/dependencies` is a magic snowflake which determines
versions of a zillion of other packages.

Fixes: 69c92de Fix git version reporting of `CzechLight/dependencies`
Fixes: 32ca34f Stop pinning `CzechLight/dependencies` via `cla-sysrepo` and `netconf-cli`
Change-Id: Iac701db50f5d349a053aa548c9ce4b960349e95a

board/czechlight/common/os-release.sh
@@ -11,10 +11,12 @@ function czechlight_query_local_make_var {
 CLA_BR2_EXTERNAL_REV=$(czechlight_describe_git ${BR2_EXTERNAL_CZECHLIGHT_PATH})
 CLA_SYSREPO_REV=$(czechlight_describe_git $(czechlight_query_local_make_var CLA_SYSREPO_OVERRIDE_SRCDIR))
 NETCONF_CLI_REV=$(czechlight_describe_git $(czechlight_query_local_make_var NETCONF_CLI_OVERRIDE_SRCDIR))
-CLA_CPP_DEPENDENCIES_REV=$(czechlight_describe_git ${BR2_EXTERNAL_CZECHLIGHT_PATH}/submodules/dependencies)
 GAMMARUS_REV=$(czechlight_describe_git $(czechlight_query_local_make_var GAMMARUS_OVERRIDE_SRCDIR))
 VELIA_REV=$(czechlight_describe_git $(czechlight_query_local_make_var VELIA_OVERRIDE_SRCDIR))
 ROUSETTE_REV=$(czechlight_describe_git $(czechlight_query_local_make_var ROUSETTE_OVERRIDE_SRCDIR))
+
+# CzechLight/dependencies might come either from a git submodule, or from a Zuul change enqueued before this one
+CLA_CPP_DEPENDENCIES_REV=$(czechlight_describe_git $(czechlight_query_local_make_var LIBYANG_OVERRIDE_SRCDIR)/..)
 
 sed -i \
 	-e 's/^VERSION_ID=/BUILDROOT_VERSION_ID=/' \

ci/build.sh
@@ -54,6 +54,7 @@ if [[ ${TRIGGERED_VIA_DEP} == 1 ]]; then
 
             # `make ${pkg}-reconfigure` is *not* enough with BR2_PER_PACKAGE_DIRECTORIES=y
             # Even this is possibly fragile if these packages were not the "leaf" ones (in terms of BR-level dependencies).
+            # But any change to CzechLight/dependencies is handled explicitly below.
             rm -rf build/${PROJECT}-custom/ per-package/${PROJECT}/
         fi
     done
@@ -68,13 +69,14 @@ fi
 # After a "big" update there should always be a standalone sync to `br2-external` as the very last step anyway.
 HAS_CHANGE_OF_DEPENDENCIES=$(jq < ~/zuul-env.json -r '[.items[]? | select(.project.name == "CzechLight/dependencies")][-1]?.project.src_dir + ""')
 if [[ ! -z "${HAS_CHANGE_OF_DEPENDENCIES}" ]]; then
-for PROJECT in \
-	libyang sysrepo libnetconf2 netopeer2 \
-	libyang-cpp sysrepo-cpp \
-	docopt-cpp replxx cppcodec sdbus-cpp \
-	; do
-    rm -rf build/{,host-}${PROJECT}-custom/ per-package/{,host-}${PROJECT}/
-done
+    sed -i "s|${ZUUL_PROJECT_SRC_DIR}/submodules/${PROJECT}|${HOME}/${HAS_CHANGE_OF_DEPENDENCIES}|g" local.mk
+    for PROJECT in \
+            libyang sysrepo libnetconf2 netopeer2 \
+            libyang-cpp sysrepo-cpp \
+            docopt-cpp replxx cppcodec sdbus-cpp \
+            ; do
+        rm -rf build/{,host-}${PROJECT}-custom/ per-package/{,host-}${PROJECT}/
+    done
 fi
 
 make source -j${CI_PARALLEL_JOBS} --output-sync=target