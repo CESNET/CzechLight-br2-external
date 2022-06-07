04eb7379f5e2a72c9d254887b96bae0943c41f99
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-06-07 08:03:04 +0200

    czechlight-cfg-fs: migration scripts and tests
    Sometimes we need to make alterations to the startup datastore (for
    instance when new feature to the YANG model is added). This commit
    introduces a migration script that is to be run after the machine boots.
    
    The persistent /cfg now stores not just the data, but also the system
    SW version which produced the stored configuration. Upon boot, the
    current system looks at the previous config (the startup.json file
    located at /cfg/sysrepo) and its associated system version
    (/cfg/sysrepo/version), and if any changes need to happen, they are
    automatically applied.
    
    This commit introduces first migration.
    This initial content makes sure that there's something (like a default
    channel plan, or some mandatory YANG data nodes) when booting from
    factory defaults (or from a pre-versioning SW release). Since this is a
    one-shot operation, the operator is free to overwrite the data (e.g.,
    rewrite the channel plan), and the system will happily accept the new data
    as long as they pass the YANG validation. This is different from the
    previous behavior of the SW stack which would apply the defaults during
    each boot, even when the configuration was restored.
    
    We also introduce tests for these migrations scripts. They are located
    under tests/czechlight-cfg-fs directory and there is a pytest runner.
    Each test case is a single directory containing current startup DS
    content (as JSON file), expected content after running migrations,
    and three control files: One for current startup DS version (so we know
    which migrations to run), one for cmdline (so both scripts
    czechlight-install-yang and czechlight-migrate can distinguish between
    czechlight box types in code), and xpath file used only if we want to
    check for a part of the sysrepo DS content.
    
    The tests simulate the workflow that is done when the system boots.
    I.e., they install YANG models, restore startup DS backup into sysrepo
    and then they run the migrations if necessary.
    
    Change-Id: I96191630fd6b3a1ffbc5ba9f62c4a6c14ab5ed92

---
+++ tests/czechlight-cfg-fs/migrations.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+import json
+import re
+import os
+import pathlib
+import shutil
+import subprocess
+import sys
+
+import pytest
+
+
+SCRIPT_ROOT = pathlib.Path(__file__).resolve().parent
+BR2_ROOT = (SCRIPT_ROOT / '../../').resolve()
+
+INSTALL_SCRIPT_PATH = BR2_ROOT / 'package/czechlight-cfg-fs/czechlight-install-yang.sh'
+MIGRATE_SCRIPT_PATH = BR2_ROOT / 'package/czechlight-cfg-fs/czechlight-migrate.sh'
+MIGRATE_DEFINITIONS_PATH = BR2_ROOT / 'package/czechlight-cfg-fs/czechlight-migration-list.sh'
+NETOPEER_SCRIPT_PATH = BR2_ROOT / 'submodules/buildroot/package/netopeer2/setup.sh'
+
+CLA_SYSREPO_PATH = BR2_ROOT / 'submodules/cla-sysrepo'
+VELIA_PATH = BR2_ROOT / 'submodules/velia'
+NETOPEER2_PATH = BR2_ROOT / 'submodules/dependencies/Netopeer2'
+
+
+def run_and_wait(ctx, desc, command_args):
+    print(f'executing {desc}')
+    with subprocess.Popen(command_args, stdout=sys.stdout, stderr=sys.stderr, env=ctx.get_env()) as proc:
+        proc.wait()
+        assert proc.returncode == 0
+
+
+class SysrepoFixture:
+    def __init__(self, test_directory, tmp_path):
+        test_directory = SCRIPT_ROOT / test_directory
+        self.test_name = test_directory.name
+
+        self.expected_file = test_directory / 'expected.json'
+        assert self.expected_file.is_file()
+
+        startup = test_directory / 'startup.json'
+        assert startup.is_file()
+
+        self.proc_cmdline = test_directory / 'cmdline'
+        assert self.proc_cmdline.is_file()
+
+        version_file = test_directory / 'version'
+        assert version_file.is_file()
+
+        tested_xpath_file = test_directory / 'xpath'
+        self.tested_xpath = tested_xpath_file.read_text() if tested_xpath_file.is_file() else None
+
+        self._running_directory = tmp_path / self.test_name
+        self._running_directory.mkdir()
+
+        self.startup_file = self._running_directory / 'startup.json'
+        shutil.copyfile(startup, self.startup_file)
+
+        self.export_file = self._running_directory / 'export.json'
+
+        self.version_file = self._running_directory / 'version'
+        shutil.copy(version_file, self.version_file)
+
+    def get_env(self):
+        res = os.environ.copy()
+        res['SYSREPO_SHM_PREFIX'] = self.test_name
+        res['SYSREPO_REPOSITORY_PATH'] = self._running_directory / 'sysrepo_repository'
+        res['CLA_YANG'] = CLA_SYSREPO_PATH / 'yang'
+        res['VELIA_YANG'] = VELIA_PATH / 'yang'
+        res['PROC_CMDLINE'] = self.proc_cmdline
+        res['CFG_VERSION_FILE'] = self.version_file
+        res['CFG_STARTUP_FILE'] = self.startup_file
+        res['NP2_MODULE_DIR'] = NETOPEER2_PATH / 'modules'
+        res['NP2_MODULE_PERMS'] = '0600'
+        res['USER'] = os.getlogin()
+        return res
+
+
+@pytest.fixture(scope='session')
+def max_version():
+    """
+    Fetches last version from czechlight-migrate script by sourcing the
+    migration definitions file and verifying the length of the migration
+    files array.
+    """
+    args = ["/bin/bash", "-c", "source " + str(MIGRATE_DEFINITIONS_PATH) + " && echo ${#MIGRATION_FILES[@]}"]
+    with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+        proc.wait()
+        stdout, stderr = proc.communicate()
+        assert stderr.decode().strip() == ''
+        assert proc.returncode == 0
+
+    return int(stdout.decode().strip())
+
+
+@pytest.fixture
+def sysrepo_fixture(request, tmp_path):
+    return SysrepoFixture(pathlib.Path(SCRIPT_ROOT / 'data' / request.param), tmp_path)
+
+
+def find_test_directories():
+    return [pytest.param(dirname) for dirname in os.listdir(path=SCRIPT_ROOT / 'data')]
+
+
+@pytest.mark.parametrize("sysrepo_fixture", find_test_directories(), indirect=True)
+def test(sysrepo_fixture, max_version):
+    # prepare sysrepo
+    run_and_wait(sysrepo_fixture, 'netopeer2 setup.sh', [NETOPEER_SCRIPT_PATH])
+    run_and_wait(sysrepo_fixture, 'czechlight-install-yang.sh', [INSTALL_SCRIPT_PATH])
+    run_and_wait(sysrepo_fixture, 'restoring startup.json to sysrepo', ['sysrepocfg', '--datastore', 'startup', '--format', 'json', f'--import={sysrepo_fixture.startup_file}'])
+
+    current_version = int(sysrepo_fixture.version_file.read_text())
+
+    # perform the actual migration
+    print(f'migration: current version is {current_version}')
+    print('migration: applying migration script')
+    run_and_wait(sysrepo_fixture, 'migration', [MIGRATE_SCRIPT_PATH])
+
+    after_migration_version = int(sysrepo_fixture.version_file.read_text())
+    assert after_migration_version == max_version
+
+    print('migration: checking datastore contents')
+    export_args = ['sysrepocfg', '--datastore', 'startup', '-f', 'json', f'--export={sysrepo_fixture.export_file}']
+    if sysrepo_fixture.tested_xpath:
+        export_args += ['-x', sysrepo_fixture.tested_xpath]
+    run_and_wait(sysrepo_fixture, 'export', export_args)
+
+    with open(sysrepo_fixture.export_file, 'r') as fp_actual:
+        with open(sysrepo_fixture.expected_file, 'r') as fp_expected:
+            print(f'migration: comparing files {sysrepo_fixture.startup_file.name} and {sysrepo_fixture.expected_file.name}')
+
+            actual = json.load(fp_actual)
+            expected = json.load(fp_expected)
+            assert actual == expected
