7b34e63ace734e51a9e0c31e2a5960b1fdbe3ae2
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-06-09 21:44:22 +0200

    czechlight-cfg-fs: install migrations services
    Change-Id: Ib16fca53e5e05421633326ce2c11d4bb928e8bfe

--- package/czechlight-install-yang/czechlight-install-yang.mk
+++ package/czechlight-install-yang/czechlight-install-yang.mk
@@ -2,10 +2,15 @@ CZECHLIGHT_INSTALL_YANG_DEPENDENCIES = systemd
 
 define CZECHLIGHT_INSTALL_YANG_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0755 -t $(TARGET_DIR)/usr/libexec/czechlight \
-		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/czechlight-install-yang.sh
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/czechlight-install-yang.sh \
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/czechlight-migrate.sh
+
+	$(INSTALL) -D -m 0644 -t $(TARGET_DIR)/usr/libexec/czechlight/migrations \
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/migrations/*
 
 	$(INSTALL) -D -m 0644 -t $(TARGET_DIR)/usr/lib/systemd/system/ \
-		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/czechlight-install-yang.service
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/czechlight-install-yang.service \
+		$(BR2_EXTERNAL_CZECHLIGHT_PATH)/package/czechlight-install-yang/czechlight-migrate.service
 endef
 
 $(eval $(generic-package))
