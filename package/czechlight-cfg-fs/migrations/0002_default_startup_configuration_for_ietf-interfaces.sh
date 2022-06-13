126ae347dacbe7e3a469494987ec178f15a1ffb3
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-06-13 15:45:30 +0200

    migration: add default configuration for ietf-interfaces
    The current state of startup datastore for ietf-interfaces model did not
    correspond to the real state of the box (its systemd-networkd
    configuration). The datastore was empty, but its configuration was, by
    default, set to "bridge all links" and set DHCP for both IPv4 and IPv6
    for br0 (the bridge).
    
    This migration should fix that. It overwrites the current networking
    content in startup datastore with the values of the default
    configuration described in the previous paragraph.
    
    Change-Id: I0b809a39044bf6bcba4135a3456c684d94ef8b86

---
+++ package/czechlight-cfg-fs/migrations/0002_default_startup_configuration_for_ietf-interfaces.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# setup default configuration for ietf-interfaces in startup DS
+# This overwrites current networking setup
+
+set -x
+set -e
+
+if [[ ${YANG_INLINE} == 1 ]]; then
+    sysrepocfg --datastore=startup --format=json --module=ietf-interfaces --import="${MIGRATIONS_DIRECTORY}/0002_ietf-interfaces_default-startup-config_sdn-inline.json"
+else
+    sysrepocfg --datastore=startup --format=json --module=ietf-interfaces --import="${MIGRATIONS_DIRECTORY}/0002_ietf-interfaces_default-startup-config.json"
+fi
