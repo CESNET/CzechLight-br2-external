CESNET/CzechLight-br2-external

commit 9f072077e1a95f5e8963962c3c0220ff067e7233

CI: make the build safer
The CI builds used to cache the build directory when the build job was
invoked indirectly, via a change to another repo. That was not working
well; there were situations where a change made via
`submodules/dependencies` was not picked up properly. Also, the caching
itself took about 10 minutes, which kinda defeats the point of caching
in the first place.

Fix this by always performing a full rebuild from scratch, with no
previous state whatsoever.

Also, add some safeguards:

- show exactly what gets picked at the level of Buildroot (yay Makefile
  and shell magic)
- reject any changes brought in transitively *iff* we're changing
  br2-external and if that change doesn't sync the submodule which was
  pulled in

Change-Id: Ief4ac98218c7f6801fd303cf0015789535edcaf5

ci/build.sh
@@ -36,14 +36,6 @@ fi
 echo BR2_PRIMARY_SITE=\"https://object-store.cloud.muni.cz/swift/v1/ci-artifacts-public/mirror/buildroot\" >> .config
 
 if [[ ${TRIGGERED_VIA_DEP} == 1 ]]; then
-    ARTIFACT_URL=$(jq < ~/zuul-env.json -r '[.artifacts[]? | select(.name == "br2-work-dir") | select(.project == "CzechLight/br2-external")][-1]?.url + ""')
-    if [[ -z "${ARTIFACT_URL}" ]]; then
-        # no job ahead, try to use git commit ID
-        ARTIFACT_URL="https://object-store.cloud.muni.cz/swift/v1/ci-artifacts-${ZUUL_TENANT}/${ZUUL_GERRIT_HOSTNAME}/CzechLight/br2-external/${ZUUL_JOB_NAME}/br2-work-dir-${BR2_EXTERNAL_COMMIT}.tar.zst"
-    fi
-    # We don't use gating, so there's a risk that there's no prebuilt artifact, so don't die if we cannot download that file
-    curl ${ARTIFACT_URL} | unzstd --stdout | tar -xf - || echo "No Buildroot prebuilt tarball found, will build from scratch"
-
     for PROJECT in cla-sysrepo netconf-cli gammarus velia rousette; do
         # If there's a change for ${PROJECT} queued ahead, ensure it gets used.
         # This means that if our submodules still pin, say, `cla-sysrepo` to some ancient version and we're testing a `netconf-cli` change,
@@ -51,11 +43,6 @@ if [[ ${TRIGGERED_VIA_DEP} == 1 ]]; then
         DEPSRCDIR=$(jq < ~/zuul-env.json -r "[.items[]? | select(.project.name == \"CzechLight/${PROJECT}\")][-1]?.project.src_dir + \"\"")
         if [[ ! -z "${DEPSRCDIR}" ]]; then
             sed -i "s|${ZUUL_PROJECT_SRC_DIR}/submodules/${PROJECT}|${HOME}/${DEPSRCDIR}|g" local.mk
-
-            # `make ${pkg}-reconfigure` is *not* enough with BR2_PER_PACKAGE_DIRECTORIES=y
-            # Even this is possibly fragile if these packages were not the "leaf" ones (in terms of BR-level dependencies).
-            # But any change to CzechLight/dependencies is handled explicitly below.
-            rm -rf build/${PROJECT}-custom/ per-package/${PROJECT}/
         fi
     done
 fi
@@ -71,14 +58,37 @@ HAS_CHANGE_OF_DEPENDENCIES=$(jq < ~/zuul-env.json -r '[.items[]? | select(.proje
 if [[ ! -z "${HAS_CHANGE_OF_DEPENDENCIES}" ]]; then
     # redirect BR packages to a Zuul-injected dependency
     sed -i "s|${ZUUL_PROJECT_SRC_DIR}/submodules/dependencies|${HOME}/${HAS_CHANGE_OF_DEPENDENCIES}|g" local.mk
+fi
 
-    # persuade BR to rebuild them
-    for PROJECT in \
-            libyang sysrepo libnetconf2 netopeer2 \
-            libyang-cpp sysrepo-cpp libnetconf2-cpp \
-            replxx sdbus-cpp \
-            ; do
-        rm -rf build/{,host-}${PROJECT}-custom/ per-package/{,host-}${PROJECT}/
+# Show exact git versions of what we're going to build
+bash <<EOF
+    # Extract variables from Makefile into the env
+    eval \$(make -E "all:" -pn -f local.mk | grep _OVERRIDE_SRCDIR | sed -E "s/^(.*) = /export \\1=/")
+    for PROJ in {libyang,sysrepo,libnetconf2}{,-cpp} netopeer2 cla-sysrepo netconf-cli gammarus velia rousette; do
+        # indirect substitution involves the usual joy in bash
+        export PROJ_UCASE=\${PROJ@U}
+        export PROJ_DIR=\${PROJ_UCASE//-/_}_OVERRIDE_SRCDIR
+        cd \${!PROJ_DIR}
+        echo \${PROJ}: \$(git describe --dirty --long --always --tags)
+    done
+EOF
+
+if [[ ${TRIGGERED_VIA_DEP} == 0 ]]; then
+    # Zuul is building a direct change to br2-external. This is by definition the "last change" in the current CI job.
+    # There might be other changes for subsequent jobs, but that doesn't matter right now.
+    # Ensure that the submodules are already pinned to whatever was provided to Zuul through the `Depends-on` commit footers.
+    # In other words, it's an error if we push a change to br2-external which `Depends-on` on a change of some other repo,
+    # but that other repo is not explicitly synced in that change to br2-external.
+    for PROJECT in dependencies cla-sysrepo netconf-cli gammarus velia rousette; do
+        DEPSRCDIR=$(jq < ~/zuul-env.json -r "[.items[]? | select(.project.name == \"CzechLight/${PROJECT}\")][-1]?.project.src_dir + \"\"")
+        if [[ ! -z "${DEPSRCDIR}" ]]; then
+            COMMIT_IN_BR2_EXT=$(cd ${ZUUL_PROJECT_SRC_DIR}/submodules/${PROJECT}; git rev-parse HEAD)
+            COMMIT_VIA_ZUUL=$(cd ${HOME}/${DEPSRCDIR}; git rev-parse HEAD)
+            if [[ ${COMMIT_IN_BR2_EXT} != ${COMMIT_VIA_ZUUL} ]]; then
+                echo "br2-external says submodules/${PROJECT} is ${COMMIT_IN_BR2_EXT}, but Zuul prepared ${COMMIT_VIA_ZUUL} instead"
+                exit 1
+            fi
+        fi
     done
 fi
 

.zuul.yaml
@@ -30,15 +30,7 @@
 - project:
     check:
       jobs:
-        - czechlight-clearfog:
-            provides: CzechLight-br2-build-clearfog
-            vars:
-              artifacts:
-                name: br2-work-dir
-                glob: 'br2-work-dir-*.tar.zst'
-            post-run: ci/post.yaml
-            roles:
-              - zuul: return-built-artifact
+        - czechlight-clearfog
 
     promote:
       jobs:

ci/post.yaml
@@ -1,3 +1,0 @@
-- hosts: all
-  roles:
-    - return-built-artifact

commit 989df0ca79b22646c760dc2a40b8824f70516613

CI: stop uploading the build dir to the cache
This tarball is not consumed anymore, but I forgot to remove that code
which was uploading it.

Fixes: 9f07207 (CI: make the build safer)
Change-Id: Iba210028bd1c24417417b07ea23ae8d24b7693bb

ci/build.sh
@@ -99,26 +99,5 @@ mv images/update.raucb ~/zuul-output/artifacts/
 
 PATH="$PATH:$(pwd)/host/bin/" pytest -vv ${ZUUL_PROJECT_SRC_DIR}/tests/czechlight-cfg-fs/migrations.py
 
-if [[ "${ZUUL_JOB_NAME}" =~ clearfog ]]; then
-    if [[ ${TRIGGERED_VIA_DEP} != 1 ]]; then
-        # store a cached tarball as an artifact
-        ARTIFACT=br2-work-dir-${BR2_EXTERNAL_COMMIT}.tar.zst
-        # everything but local.mk which we might have adjusted in job prologue, so let's not overwrite that
-        tar --totals -c \
-            --exclude='images/rootfs.*' \
-            --exclude='images/sdcard.*' \
-            --exclude='images/usb-flash.*' \
-            .br* \
-            build \
-            .config \
-            host \
-            images \
-            Makefile \
-            per-package \
-            target \
-            | zstd -T0 > ~/zuul-output/artifacts/${ARTIFACT}
-    fi
-fi
-
 # TODO: USB image as well? (`fallocate -d` to make it sparse)
 # TODO: make legal-info