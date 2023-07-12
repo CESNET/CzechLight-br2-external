e105d265a2b57af5b56a45771bddb03cc2b07865
Jan Kundrát <jan.kundrat@cesnet.cz>
2023-07-12 17:38:16 +0200

    Sync to the F38 version
    This required an upgrade from nghttp2's integrated ASIO C++ bindings to
    the standalone nghttp2-asio library, which meant an upgrade to
    Buildroot. Now at the `cesnet/2023-07-12--00` branch.
    
    Change-Id: I0cf3e29bc229d905d8369521f57ec1aad69813a0
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/6355

--- package/rousette/rousette.mk
+++ package/rousette/rousette.mk
@@ -2,7 +2,7 @@ ROUSETTE_VERSION = master
 ROUSETTE_SITE = https://gerrit.cesnet.cz/CzechLight/rousette
 ROUSETTE_SITE_METHOD = git
 ROUSETTE_INSTALL_STAGING = NO
-ROUSETTE_DEPENDENCIES = boost docopt-cpp nghttp2 spdlog systemd sysrepo-cpp
+ROUSETTE_DEPENDENCIES = boost docopt-cpp nghttp2-asio spdlog systemd sysrepo-cpp
 ROUSETTE_LICENSE = Apache-2.0
 ROUSETTE_LICENSE_FILES = LICENSE.md
 
