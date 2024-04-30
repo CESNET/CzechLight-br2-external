4d708d55d7a7967bc61157e2a2b6f0d543931cbe
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-04-30 14:55:41 +0200

    Sync rousette
    Rousette needs a migration in order to allow anonymous users to read
    ietf-restconf-monitoring module.
    
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/rousette/+/7080
    Change-Id: Ia69c3c12642c71a68c77432aa929fa10e0fb77db

---
+++ package/czechlight-cfg-fs/migrations/0007_nacm_anonymous_user_monitoring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Allow NACM anonymous access user to access ietf-restconf-monitoring module
+# --------------------------------------------------------------------------
+# Adds rules for the anonymous user access before wildcard deny rule
+# if the rule doesn't exist, do nothing
+
+if WILDCARD_DENY_RULE_QUERY=$(sysrepocfg -d startup -G "/ietf-netconf-acm:nacm/rule-list[name='Permit yangnobody user/group to read only some modules']/rule[name='wildcard-deny']/name") \
+    && [ "$WILDCARD_DENY_RULE_QUERY" == "wildcard-deny" ]; then
+    sysrepocfg --datastore=startup --format=json --module=ietf-netconf-acm --edit="${MIGRATIONS_DIRECTORY}/0007_nacm_anonymous_user_monitoring.json"
+else
+    sysrepocfg --datastore=startup --format=json --module=ietf-netconf-acm --edit="${MIGRATIONS_DIRECTORY}/0007_nacm_anonymous_user_monitoring_append.json"
+fi
