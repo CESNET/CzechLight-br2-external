2ae1cd1f87d7efae11005a41636b7766bd5557f7
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-05-24 11:49:43 +0200

    YANG: simplify installation
    - Merge everything into a single `sysrepoctl` call. This should be
      faster, and it will definitely make the code much, much shorter.
    
    - Don't bother with UNIX permissions. These do not work that well with
      the NACM access rules. As long as there's no direct shell on the
      device, at least for non-root, this should work well (and we only
      offer shell to root; the unprivileged users got a NETCONF console).
    
    - Remove the unused initial data; these have moved to being taken care
      of via migrations in 04eb737 (czechlight-cfg-fs: migration scripts and
      tests). Now, I'm not entirely sure this still works for, say, the
      Coherent Add/Drop ROADM where some data are mandatory, but hey, this
      change only removes setting up some variable that's not ever used,
      anyway).
    
    Change-Id: I71de2fb63d254e3ca5b786697b62c133109fdcc3

--- package/czechlight-cfg-fs/czechlight-install-yang.sh
+++ package/czechlight-cfg-fs/czechlight-install-yang.sh
@@ -2,12 +2,6 @@
 
 set -ex
 
-YANG_ROADM=0
-YANG_COHERENT=0
-YANG_INLINE=0
-YANG_CALIBRATION=0
-YANG_BIDI=0
-
 CLA_YANG="${CLA_YANG:-/usr/share/cla-sysrepo/yang}"
 VELIA_YANG="${VELIA_YANG:-/usr/share/velia/yang}"
 ALARMS_YANG="${ALARMS_YANG:-/usr/share/sysrepo-ietf-alarms/yang}"
@@ -22,96 +16,62 @@ for ARG in $(cat "$PROC_CMDLINE"); do
     esac
 done
 
-INITIAL_DATA=${CZECHLIGHT%%-g2}
-
 case "${CZECHLIGHT}" in
     sdn-roadm-line*)
-        YANG_ROADM=1
-        WITH_FEATURE=hw-line-9
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-roadm-device@2021-03-05.yang --enable-feature hw-line-9"
         ;;
     sdn-roadm-add-drop*)
-        YANG_ROADM=1
-        WITH_FEATURE=hw-add-drop-20
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-roadm-device@2021-03-05.yang --enable-feature hw-add-drop-20"
         ;;
     sdn-roadm-hires-add-drop*)
-        YANG_ROADM=1
-        WITH_FEATURE="hw-add-drop-20 pre-wss-ocm"
-        INITIAL_DATA=sdn-roadm-add-drop
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-roadm-device@2021-03-05.yang --enable-feature hw-add-drop-20 --enable-feature pre-wss-ocm"
         ;;
     sdn-roadm-coherent-a-d*)
-        YANG_COHERENT=1
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-coherent-add-drop@2021-03-05.yang"
         ;;
     sdn-inline*)
-        YANG_INLINE=1
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-inline-amp@2021-03-05.yang"
         ;;
     calibration-box)
-        YANG_CALIBRATION=1
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-calibration-device@2019-06-25.yang"
         ;;
     sdn-bidi-cplus1572-g2)
-        YANG_BIDI=1
-        BIDI_FEATURES="dualband-c-plus-1572"
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-bidi-amp@2022-03-22.yang --enable-feature dualband-c-plus-1572"
         ;;
     sdn-bidi-cplus1572-ocm-g2)
-        YANG_BIDI=1
-        BIDI_FEATURES="dualband-c-plus-1572 c-band-ocm"
-        INITIAL_DATA=sdn-bidi-cplus1572
+        DEVICE_YANG="--install ${CLA_YANG}/czechlight-bidi-amp@2022-03-22.yang --enable-feature dualband-c-plus-1572 --enable-feature c-band-ocm"
         ;;
 esac
 
-sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/iana-hardware@2018-03-13.yang
-sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/ietf-hardware@2018-03-13.yang
-sysrepoctl --change ietf-hardware --permissions 0660 --enable-feature hardware-sensor --enable-feature hardware-state
-
-if [[ ${YANG_ROADM} == 1 ]]; then
-    FEATURE_ARGS=""
-    if [[ ${WITH_FEATURE} ]]; then
-        for FEATURE in ${WITH_FEATURE}; do
-            FEATURE_ARGS="${FEATURE_ARGS} --enable-feature ${FEATURE}"
-        done
-    fi
-    sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/czechlight-roadm-device@2021-03-05.yang ${FEATURE_ARGS} --permissions 0660
-fi
-
-if [[ ${YANG_COHERENT} == 1 ]]; then
-    sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/czechlight-coherent-add-drop@2021-03-05.yang --permissions 0660
-    sysrepoctl --change czechlight-coherent-add-drop --permissions 0660
-fi
-
-if [[ ${YANG_INLINE} == 1 ]]; then
-    sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/czechlight-inline-amp@2021-03-05.yang --permissions 0660
-fi
-
-if [[ ${YANG_CALIBRATION} == 1 ]]; then
-    sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/czechlight-calibration-device@2019-06-25.yang --permissions 0660
-fi
-
-if [[ ${YANG_BIDI} == 1 ]]; then
-    FEATURE_ARGS=""
-    for FEATURE in ${BIDI_FEATURES}; do
-        FEATURE_ARGS="${FEATURE_ARGS} --enable-feature ${FEATURE}"
-    done
-    sysrepoctl --search-dirs ${CLA_YANG} --install ${CLA_YANG}/czechlight-bidi-amp@2022-03-22.yang ${FEATURE_ARGS} --permissions 0660
-fi
-
-sysrepoctl --search-dirs ${ALARMS_YANG} --install ${ALARMS_YANG}/ietf-alarms@2019-09-11.yang --permissions 0660 --enable-feature alarm-shelving --enable-feature alarm-summary
-sysrepoctl --search-dirs ${ALARMS_YANG} --install ${ALARMS_YANG}/sysrepo-ietf-alarms@2022-02-17.yang --permissions 0660
-
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/ietf-system@2014-08-06.yang --permissions 0660
-
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/czechlight-lldp@2020-11-04.yang --permissions 0660
-
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/czechlight-system@2022-07-08.yang --permissions 0660
-
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/iana-if-type@2017-01-19.yang
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/ietf-interfaces@2018-02-20.yang --permissions 0660
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/ietf-ip@2018-02-22.yang --permissions 0660
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/ietf-routing@2018-03-13.yang --permissions 0660
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/ietf-ipv4-unicast-routing@2018-03-13.yang --permissions 0660
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/ietf-ipv6-unicast-routing@2018-03-13.yang --permissions 0660
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/czechlight-network@2021-02-22.yang --permissions 0660
-
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/czechlight-firewall@2021-01-25.yang --permissions 0600
-sysrepoctl --change ietf-access-control-list --enable-feature match-on-eth --enable-feature eth --enable-feature match-on-ipv4 --enable-feature ipv4 --enable-feature match-on-ipv6 --enable-feature ipv6 --enable-feature mixed-eth-ipv4-ipv6
-
-sysrepoctl --search-dirs ${VELIA_YANG} --install ${VELIA_YANG}/velia-alarms@2022-07-12.yang --permissions 0660
-sysrepoctl --search-dirs ${ROUSETTE_YANG} --install ${ROUSETTE_YANG}/ietf-restconf@2017-01-26.yang
+sysrepoctl \
+    --search-dirs ${CLA_YANG}:${VELIA_YANG}:${ALARMS_YANG}:${ROUSETTE_YANG} \
+    --install ${CLA_YANG}/iana-hardware@2018-03-13.yang \
+    --install ${CLA_YANG}/ietf-hardware@2018-03-13.yang \
+        --enable-feature hardware-sensor \
+        --enable-feature hardware-state \
+    --install ${ALARMS_YANG}/ietf-alarms@2019-09-11.yang \
+        --enable-feature alarm-shelving \
+        --enable-feature alarm-summary \
+    --install ${ALARMS_YANG}/sysrepo-ietf-alarms@2022-02-17.yang \
+    --install ${VELIA_YANG}/ietf-system@2014-08-06.yang \
+    --install ${VELIA_YANG}/czechlight-lldp@2020-11-04.yang \
+    --install ${VELIA_YANG}/czechlight-system@2022-07-08.yang \
+    --install ${VELIA_YANG}/iana-if-type@2017-01-19.yang \
+    --install ${VELIA_YANG}/ietf-interfaces@2018-02-20.yang \
+    --install ${VELIA_YANG}/ietf-ip@2018-02-22.yang \
+    --install ${VELIA_YANG}/ietf-routing@2018-03-13.yang \
+    --install ${VELIA_YANG}/ietf-ipv4-unicast-routing@2018-03-13.yang \
+    --install ${VELIA_YANG}/ietf-ipv6-unicast-routing@2018-03-13.yang \
+    --install ${VELIA_YANG}/czechlight-network@2021-02-22.yang \
+    --install ${VELIA_YANG}/ietf-access-control-list@2019-03-04.yang \
+        --enable-feature match-on-eth \
+        --enable-feature eth \
+        --enable-feature match-on-ipv4 \
+        --enable-feature ipv4 \
+        --enable-feature match-on-ipv6 \
+        --enable-feature ipv6 \
+        --enable-feature mixed-eth-ipv4-ipv6 \
+    --install ${VELIA_YANG}/czechlight-firewall@2021-01-25.yang \
+    --install ${VELIA_YANG}/velia-alarms@2022-07-12.yang \
+    --install ${ROUSETTE_YANG}/ietf-restconf@2017-01-26.yang \
+    ${DEVICE_YANG}
