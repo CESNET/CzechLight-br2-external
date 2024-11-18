CESNET/CzechLight-br2-external

commit d08ced2ac1af6b434da2ef9d7e5013af16c041d6

unbreak booting within the flash-via-USB image
Our rootfs has grown, and as a result, the default XZ-compressed image
(in the version that's suitable for U-Boot's consumption) no longer fits
into whatever RAM limits are in effect on this platform. Here's how it
looks during boot:

[    3.431410] sd 4:0:0:0: [sda] 2053120 512-byte logical blocks: (1.05 GB/1003 MiB)
[    3.439181] sd 4:0:0:0: [sda] Write Protect is off
[    3.444221] sd 4:0:0:0: [sda] No Caching mode page found
[    3.449562] sd 4:0:0:0: [sda] Assuming drive cache: write through
[    3.457877]  sda: sda1
[    3.460482] sd 4:0:0:0: [sda] Attached SCSI removable disk
[    4.439737] Initramfs unpacking failed: write error
[    4.517106] Freeing initrd memory: 147888K
[    4.522280] Freeing unused kernel image (initmem) memory: 1024K
[    4.528428] Run /init as init process
/init: [    4.540735] Kernel panic - not syncing: Attempted to kill init! exitcode=0x00007f00
exec: line 15: /[    4.548977] CPU0: stopping
[    4.548982] CPU: 0 PID: 0 Comm: swapper/0 Not tainted 6.6.54-10-g79692ad15f4e1 #1
[    4.548987] Hardware name: Marvell Armada 380/385 (Device Tree)
[    4.548994]  unwind_backtrace from show_stack+0x10/0x14
[    4.549010]  show_stack from dump_stack_lvl+0x40/0x4c
[    4.549021]  dump_stack_lvl from do_handle_IPI+0x18c/0x1c0
[    4.549030]  do_handle_IPI from ipi_handler+0x18/0x20
[    4.549037]  ipi_handler from handle_percpu_devid_irq+0x8c/0x1d8
[    4.549046]  handle_percpu_devid_irq from generic_handle_domain_irq+0x28/0x38
[    4.549057]  generic_handle_domain_irq from gic_handle_irq+0x74/0x88
[    4.549067]  gic_handle_irq from generic_handle_arch_irq+0x34/0x44
[    4.549076]  generic_handle_arch_irq from __irq_svc+0x88/0xb0
[    4.549083] Exception stack(0xc0f01f28 to 0xc0f01f70)
[    4.549089] 1f20:                   00008324 00000001 00000000 00000000 c0f09040 c104f6d0
[    4.549093] 1f40: 00000000 c0f04fb0 00000000 00000000 c0e45a6c 00000000 c104efc8 c0f01f78

configs/czechlight_clearfog_defconfig
@@ -86,7 +86,7 @@ BR2_PACKAGE_SYSTEMD_RANDOMSEED=y
 BR2_PACKAGE_VIM=y
 # BR2_PACKAGE_VIM_RUNTIME is not set
 BR2_TARGET_ROOTFS_CPIO=y
-BR2_TARGET_ROOTFS_CPIO_GZIP=y
+BR2_TARGET_ROOTFS_CPIO_XZ=y
 BR2_TARGET_ROOTFS_CPIO_UIMAGE=y
 BR2_TARGET_ROOTFS_EXT2=y
 BR2_TARGET_ROOTFS_EXT2_4=y

commit 06db05ad6827afa8fa8bc1233b91dfce08916f7e

unbreak USB flashing, take two
There's a memory limit of "half the RAM" which applies to the sum of
sizes of the compressed and uncompressed CPIO archive of the rootfs. We
have been working dangerously close to this limit, and had to enable a
different compression recently just to buy us some time. This time has
run out recently, and now the board won't boot again, as reported by
Jarek.

We could have solved this by building a second standalone, minimal
rootfs, but I was too lazy to do that. Instead, simply filter the list
of files in the rootfs, selectively excluding the largest heavies from
the resulting CPIO image.

The change for Buildroot is available in our branch `cesnet/2024-11-15`.

Fixes: d08ced2a unbreak booting within the flash-via-USB image
Change-Id: If3962d80303cd5ddca6081e01e3054e97865cede

board/czechlight/clearfog/usb-cpio-ignored.sh
@@ -1,0 +1,8 @@
+#!/usr/bin/env sh
+grep -v \
+    -e 'usr/bin/cla-.*$' \
+    -e 'usr/bin/netconf-cli$' \
+    -e 'usr/bin/sysrepo-cli$' \
+    -e 'usr/bin/rousette$' \
+    -e 'usr/bin/veliad-.*$' \
+    -e 'usr/bin/sysrepo-ietf-alarmsd$'

configs/czechlight_clearfog_defconfig
@@ -86,6 +86,8 @@ BR2_PACKAGE_SYSTEMD_RANDOMSEED=y
 BR2_PACKAGE_VIM=y
 # BR2_PACKAGE_VIM_RUNTIME is not set
 BR2_TARGET_ROOTFS_CPIO=y
+BR2_TARGET_ROOTFS_CPIO_FILTER=y
+BR2_TARGET_ROOTFS_CPIO_FILTER_SCRIPT="$(BR2_EXTERNAL_CZECHLIGHT_PATH)/board/czechlight/clearfog/usb-cpio-ignored.sh"
 BR2_TARGET_ROOTFS_CPIO_XZ=y
 BR2_TARGET_ROOTFS_CPIO_UIMAGE=y
 BR2_TARGET_ROOTFS_EXT2=y