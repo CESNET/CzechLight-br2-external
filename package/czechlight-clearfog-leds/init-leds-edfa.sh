f974c91013184c037082ee2ae40dec2b9e7ee99c
Jan Kundrát <jan.kundrat@cesnet.cz>
2025-01-29 17:19:19 +0100

    fix LED labels on various types of HW
    This is a bit messy, I'm afraid. The only part which is common across
    all device types is the PCB. According to the CAD data, its labeling
    uses both D1/D2/... *and* the friendly labels such as STATUS and UID.
    These friendly labels do not match labels on the case in some cases,
    though. The silkscreen on the metal enclosure "almost always" has a
    human-readable label which matches the LED's function. These functions
    are different over different HW models, and as a not-so-nice bonus, some
    inline amps use different labeling schemes, maybe due to some HW reuse
    in the past.
    
    So, let's setup the common parts in the "inner" DTSI file, *and* use
    some DT-level labels to enable its overriding in the "outer" DT files.
    Once these labels (which are visible in sysfs as well) are set up, let's
    configure the operation from the userspace init script which sets up
    proper kernel-level LED triggers.
    
    Change-Id: I16f598e8e0b48e0fa71c35440645efedca7862f1

--- package/czechlight-clearfog-leds/init-leds-edfa.sh
+++ package/czechlight-clearfog-leds/init-leds-edfa.sh
@@ -4,26 +4,44 @@ set -ex
 
 # Green LED for "we're getting some signal"
 
-if grep -q '\<czechlight=sdn-inline\>' /proc/cmdline; then
-    # LEDs have different labels on this HW
-    STAGE1_LED=uid:green
-    STAGE2_LED=line:green
+if grep -q '\<czechlight=sdn-roadm-line\>' /proc/cmdline; then
+    EDFA1_STAGE1_LED=line:green
+elif grep -q '\<czechlight=sdn-inline\>' /proc/cmdline; then
+    EDFA1_STAGE1_LED=line-west:green
+    EDFA1_STAGE2_LED=line-east:green
+elif \
+        grep -q '\<czechlight=sdn-roadm-add-drop\>' /proc/cmdline || \
+        grep -q '\<czechlight=sdn-roadm-coherent-a-d\>' /proc/cmdline || \
+        grep -q '\<czechlight=sdn-roadm-hires-add-drop\>' /proc/cmdline \
+        ; then
+    EDFA1_STAGE1_LED=express:green
+elif grep -q '\<czechlight=sdn-bidi-cplus1572\>' /proc/cmdline; then
+    EDFA1_STAGE1_LED=line-c:green
+    EDFA2_STAGE1_LED=line-l:green
 else
-    # Line/Degree: signal present at Line IN
-    # WSS A/D and Coherent A/D: signal present at some Express IN
-    STAGE1_LED=line:green
+    echo "No EDFA LEDs recognized"
+    exit 1
 fi
 
-if [[ -n "${STAGE1_LED+set}" ]]; then
-    cd /sys/class/leds/${STAGE1_LED}
+if [[ -n "${EDFA1_STAGE1_LED+set}" ]]; then
+    cd /sys/class/leds/${EDFA1_STAGE1_LED}
     echo gpio > trigger
     GPIO=$(sed -En 's/.*gpio-(.*) \(EDFA1_ST1_IN_LOS_A .*/\1/p' /sys/kernel/debug/gpio)
     echo $GPIO > gpio
 fi
 
-if [[ -n "${STAGE2_LED+set}" ]]; then
-    cd /sys/class/leds/${STAGE2_LED}
+if [[ -n "${EDFA1_STAGE2_LED+set}" ]]; then
+    cd /sys/class/leds/${EDFA1_STAGE2_LED}
     echo gpio > trigger
     GPIO=$(sed -En 's/.*gpio-(.*) \(EDFA1_ST2_IN_LOS_A .*/\1/p' /sys/kernel/debug/gpio)
     echo $GPIO > gpio
 fi
+
+if [[ -n "${EDFA2_STAGE1_LED+set}" ]]; then
+    cd /sys/class/leds/${EDFA2_STAGE1_LED}
+    echo gpio > trigger
+    GPIO=$(sed -En 's/.*gpio-(.*) \(EDFA2_ST1_IN_LOS_A .*/\1/p' /sys/kernel/debug/gpio)
+    echo $GPIO > gpio
+fi
+
+# there's no EDFA2, stage2 in any of our HW, so no EDFA2_STAGE2_LED block here
