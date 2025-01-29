d2c8e650e7d7926924c0716885e52cbf5bb37eed
Jan Kundrát <jan.kundrat@cesnet.cz>
2025-01-29 17:19:19 +0100

    fix LED labels on various types of HW
    This is a bit messy, I'm afraid. The only part which is common across
    all device types is the PCB. According to the CAD data, its labeling
    uses both D1/D2/... *and* the friendly labels such as STATUS and UID.
    These friendly labels do not match labels on the case in some cases,
    though. The silkscreen on the metal enclosure "almost always" has a
    human-readable label which matches the LED's function. These functions
    are different over different HW models, and as a not-so-nice bonus, some
    inline amps use different labeling schemes, maybe due to some HW reuse
    in the past.
    
    So, let's setup the common parts in the "inner" DTSI file, *and* use
    some DT-level labels to enable its overriding in the "outer" DT files.
    Once these labels (which are visible in sysfs as well) are set up, let's
    configure the operation from the userspace init script which sets up
    proper kernel-level LED triggers.
    
    Change-Id: I16f598e8e0b48e0fa71c35440645efedca7862f1

--- package/czechlight-clearfog-leds/clearfog-test-leds.sh
+++ package/czechlight-clearfog-leds/clearfog-test-leds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LEDS=(NONE led5:{red,green,blue} status:{red,green,blue} uid:{red,green,blue} line:{red,green,blue} sfp:{red,green,blue} port1:{red,green} port2:{red,green} port3:{red,green} port4:{red,green} port5:{red,green} port6:{red,green} port7:{red,green} port8:{red,green})
+LEDS=(NONE led5:{red,green,blue} status:{red,green,blue} uid:{red,green,blue} line:{red,green,blue} express:{red,green,blue} line-west:{red,green,blue} line-east:{red,green,blue} line-c:{red,green,blue} line-l:{red,green,blue} sfp:{red,green,blue} port1:{red,green} port2:{red,green} port3:{red,green} port4:{red,green} port5:{red,green} port6:{red,green} port7:{red,green} port8:{red,green})
 
 LAST=${#LEDS[@]}
 let "LAST2 = $LAST - 1"
@@ -14,5 +14,9 @@ for NUM in $(seq 1 ${LAST}); do
     if [[ $NUM -ne $LAST ]]; then
         echo 256 > "/sys/class/leds/${LEDS[$NUM]}/brightness"
     fi
-    sleep 0.3
+    if [[ ! -d "/sys/class/leds/${LEDS[$NUM]}" ]]; then
+        echo "No LED ${LEDS[$NUM]}"
+    else
+        sleep 0.3
+    fi
 done
