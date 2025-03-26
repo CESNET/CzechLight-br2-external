04b3b72322dedad965728b3efc99e91d7bfba1fa
Jan Kundrát <jan.kundrat@cesnet.cz>
2025-03-26 17:01:25 +0100

    LED: use left-to-right order when testing
    Change-Id: If96c3579d467a44c88953de70c951f6326e4b9f3

--- package/czechlight-clearfog-leds/clearfog-test-leds.sh
+++ package/czechlight-clearfog-leds/clearfog-test-leds.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-LEDS=(NONE led5:{red,green,blue} status:{red,green,blue} uid:{red,green,blue} line:{red,green,blue} express:{red,green,blue} line-west:{red,green,blue} line-east:{red,green,blue} line-c:{red,green,blue} line-l:{red,green,blue} sfp:{red,green,blue} port1:{red,green} port2:{red,green} port3:{red,green} port4:{red,green} port5:{red,green} port6:{red,green} port7:{red,green} port8:{red,green})
+for LED in /sys/class/leds/*/brightness; do
+    echo 0 > $LED
+done
+sleep 1
+
+LEDS=(NONE led5:{red,green,blue} line-c:{red,green,blue} line-west:{red,green,blue} status:{red,green,blue} uid:{red,green,blue} line:{red,green,blue} express:{red,green,blue} line-east:{red,green,blue} line-l:{red,green,blue} sfp:{red,green,blue} port1:{red,green} port2:{red,green} port3:{red,green} port4:{red,green} port5:{red,green} port6:{red,green} port7:{red,green} port8:{red,green})
 
 LAST=${#LEDS[@]}
 let "LAST2 = $LAST - 1"
