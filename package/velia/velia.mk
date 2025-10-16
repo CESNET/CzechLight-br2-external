CESNET/CzechLight-br2-external

commit e950cf7aa2d9309c23380bf76606871ca5c3b56d

libyang v3
This is a huge commit which changes a lot of things in the system,
including the boot process and configuration handling. There are also
massive updates all over the stack which have accumulated in the
upstream projects. In no particular order:

- The configuration schema has changed in an incompatible way. That's
  due to an update of some drafts related to the ietf-netconf-server
  YANG model and its dependencies; as a result, the way how SSH keys and
  SSH listening configuration is managed in YANG data is incompatible
  with the previous approach, and that means that we *cannot* import the
  previous config into sysrepo which already has new YANG modules.
  "Solve" that by simply dropping the entire YANG config (!) because we
  decided that it's sometimes better to start from scratch. Yay.

- Config migrations now work at the JSON level, not through sysrepo.
  There is currently no real "migration" apart from the initial one (the
  one that brings the config schema from the implicit/empty "v0" to
  "v9"). The TL;DR version is that future migrations will work on an
  JSON input, producing some new JSON output, and only after the
  migration is done, we push stuff into sysrepo. Also, the migrations
  will (likely) not be performed in a sequence, on a cumulative basis,
  but it is now a one-shot process with no intermediate steps. This
  should hopefully simplify the launching/wrapping code. From the
  developer's standpoint, any migration should focus on bringing the
  system to the "final state" (v_new), not to v_old + 1.

  These migrations will use `jq` heavily. So far, we only have some
  "simple" code which merges objects (and arrays!) recursively, courtesy
  of Peter Koppstein (https://stackoverflow.com/a/53666584/2245623).

- Some Buildroot options which should have been hardcoded (e.g.,
  persisting of OpenSSH server keys) are now unconditional. Once the
  czechlight-cfg-fs package is selected, these cannot be disabled.

package/velia/velia.mk
@@ -2,15 +2,12 @@ VELIA_VERSION = master
 VELIA_SITE = https://gerrit.cesnet.cz/CzechLight/velia
 VELIA_SITE_METHOD = git
 VELIA_INSTALL_STAGING = NO
-VELIA_DEPENDENCIES = docopt-cpp spdlog boost sdbus-cpp systemd sysrepo-cpp libnl json-for-modern-cpp
+VELIA_DEPENDENCIES = docopt-cpp spdlog boost sdbus-cpp systemd sysrepo-cpp libnl json-for-modern-cpp date
 VELIA_LICENSE = Apache-2.0
 VELIA_LICENSE_FILES = LICENSE.md
 
 VELIA_CONF_OPTS = \
 	-DTHREADS_PTHREAD_ARG:STRING=-pthread \
-	-DVELIA_BACKUP_ETC_SHADOW=/cfg/etc/shadow \
-	-DVELIA_BACKUP_ETC_HOSTNAME=/cfg/etc/hostname \
-	-DVELIA_AUTHORIZED_KEYS_FORMAT="/cfg/ssh-user-auth/{USER}" \
 	-DNFT_EXECUTABLE=/usr/sbin/nft \
 	-DSSH_KEYGEN_EXECUTABLE=/usr/bin/ssh-keygen \
 	-DCHPASSWD_EXECUTABLE=/usr/sbin/chpasswd \

commit 039664b55870616a05b5392cce0247a0e9ecb147

fix /cfg overrides for velia
When I've moved these overrides into another file, the order of
evaluation as done by make(1) led to a situation where the
package/czechlight-cfg-fs/czechlight-cfg-fs.mk was evaluated before
package/velia/velia.mk. As a result, these overrides which were supposed
to ensure that stuff gets saved into `/cfg/etc`, were not having any
effect.

Change-Id: Ia9f1d4b5c32046fe13f78ff7641ee1fc98902e26
Fixes: e950cf7a (libyang v3)
Bug: https://github.com/CESNET/velia/issues/16

package/velia/velia.mk
@@ -6,7 +6,7 @@ VELIA_DEPENDENCIES = docopt-cpp spdlog boost sdbus-cpp systemd sysrepo-cpp libnl
 VELIA_LICENSE = Apache-2.0
 VELIA_LICENSE_FILES = LICENSE.md
 
-VELIA_CONF_OPTS = \
+VELIA_CONF_OPTS += \
 	-DTHREADS_PTHREAD_ARG:STRING=-pthread \
 	-DNFT_EXECUTABLE=/usr/sbin/nft \
 	-DSSH_KEYGEN_EXECUTABLE=/usr/bin/ssh-keygen \