89cfd81967ded907da329b317807c924928a357d
Jan Kundrát <jan.kundrat@cesnet.cz>
2026-05-18 10:53:17 +0200

    Sync the YANG stack
    The YANG modules provided by upstream projects have changed, so the
    Buildroot packaging has been updated in the `cesnet/2026-05-18` branch.
    The change is not important, but let's save some future hassle and
    switch to version-less imports (or at least I hope this will save some
    future hassle and not cause some...).
    
    Change-Id: I6f31ba2bb544f297d734d9dad5efd30693c303e3
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/9216

--- package/czechlight-cfg-fs/cfg-yang.sh
+++ package/czechlight-cfg-fs/cfg-yang.sh
@@ -78,7 +78,7 @@ VELIA_MODULES=(
     "--install ${VELIA_YANG}/velia-alarms@2022-07-12.yang"
 )
 CFG_FS_MODULES=(
-    "--install ${CFG_FS_YANG}/czechlight-netconf-server@2025-12-01.yang"
+    "--install ${CFG_FS_YANG}/czechlight-netconf-server@2026-05-18.yang"
 )
 CLA_MODULES=(
     "--install ${CLA_YANG}/iana-hardware@2018-03-13.yang"
