ed16423e75e911c86ba52d4b6c1604b67d8e199b
Jan Kundrát <jan.kundrat@cesnet.cz>
2026-06-10 13:09:18 +0200

    Sync the YANG stack
    This adds support for the "private candidate" DS. The Buildroot
    packaging is now in the `cesnet/2026-06-10` branch.
    
    The YANG setup got even more complex today. Previously, some extra
    module installation was only needed for netopeer2, but not it's also
    required for sysrepo -- hence the Buildroot update. Unfortunately, both
    sysrepo and netopeer2 now install the `ietf-subscribed-notifications`
    module, but with a different set of YANG-level features. Because we're
    trying really, really hard to build a *single* installation command with
    all the required YANG modules, we now have to monkey-patch this crap at
    two places. Yay.
    
    So, let'streat sysrepo's modules the same way as netopeer2's modules, do
    everything at the first instance (with sysrepo, splicing in all the
    required features for both netopeer2 and rousette, and be done with it,
    right? Nope, of course, because sysrepo decided to split its in-the-tree
    YANG modules into two directories for increased fun. All these modules
    are installed into a single systemwide location, but that means that we
    have to change some env variables to make this day even more
    interesting. Yay again.
    
    Change-Id: I7199c57e32bd5bf67513c82bbd026081919e35f0
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/9224
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/rousette/+/9228
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/rousette/+/8716

--- package/czechlight-cfg-fs/czechlight-cfg-fs.mk
+++ package/czechlight-cfg-fs/czechlight-cfg-fs.mk
@@ -80,6 +80,7 @@ define CZECHLIGHT_CFG_FS_INSTALL_TARGET_CMDS
 		sysrepo-ietf-alarms.service \
 		sysrepo-persistent-cfg.service \
 		sysrepo-plugind.service \
+		sysrepo-notifd.service \
 		velia-firewall.service \
 		velia-health.service \
 		velia-system.service \
@@ -132,8 +133,7 @@ czechlight-cfg-fs-test-migrations: $(BUILD_DIR)/czechlight-cfg-fs/.stamp_configu
 		VELIA_SRCDIR=$(VELIA_SRCDIR) \
 		SYSREPO_IETF_ALARMS_SRCDIR=$(SYSREPO_IETF_ALARMS_SRCDIR) \
 		ROUSETTE_SRCDIR=$(ROUSETTE_SRCDIR) \
-		LIBNETCONF2_SRCDIR=$(LIBNETCONF2_SRCDIR) \
-		NETOPEER2_SRCDIR=$(NETOPEER2_SRCDIR) \
+		BASE_TARGET_DIR=$(BASE_TARGET_DIR) \
 		PYTHONDONTWRITEBYTECODE=1 \
 		pytest \
 			-vv \
