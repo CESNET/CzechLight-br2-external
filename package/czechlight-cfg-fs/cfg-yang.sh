CESNET/CzechLight-br2-external

commit acd772a30aafcb37b6fdd2bf59cbb4ac457a6f99

hack: temporarily switch to sysrepo-cli for DWDM management
There's a long-standing issue in the NETCONF backend of the CLI where we
are unable to easily switch between some case nodes with default values.
Let's mask that via bypassing the NETCONF layer. Not a great fix, but
this has been explicitly requested by Michal.

Bug: https://github.com/CESNET/netconf-cli/issues/26
Bug: https://github.com/CESNET/netopeer2/issues/1756
Change-Id: I7258cef7d137404e51662971fd806192fc93ceaf
Reported-by: Michal Hažlinský <hazlinsky@cesnet.cz>

board/czechlight/common/users
@@ -1,1 +1,1 @@
-dwdm -1 optics -1 =dwdm /home/dwdm /bin/netconf-cli-local - Optical management
+dwdm -1 optics -1 =dwdm /home/dwdm /bin/sysrepo-cli - Optical management

commit eeb770ed1e3e74725a9dda5725723fb77dadf070

Enable raw sysrepo access for dwdm:optics
When bypassing the NETCONF (or RESTCONF) layer and talking to sysrepo
directly, the process must be able to access the main SHM, otherwise it
will just fail immediately. This means that any user who wants to talk
to sysrepo directly must be a member of the `sysrepo` group, so this
commit fixes that for the `dwdm` user.

Furthermore, the optical modules (and HW modules, which are closely
related) were owned root:root, with permissions 0600. That meant that
the `dwdm` user would not be able to even read its content. Fix that by
using a proper group for this.

Fixes: acd772a3 hack: temporarily switch to sysrepo-cli for DWDM management
Change-Id: I4946f865f08cf320f908ac677b299f1aa622a83b

board/czechlight/common/users
@@ -1,1 +1,1 @@
-dwdm -1 optics -1 =dwdm /home/dwdm /bin/sysrepo-cli - Optical management
+dwdm -1 optics -1 =dwdm /home/dwdm /bin/sysrepo-cli sysrepo Optical management

package/czechlight-cfg-fs/cfg-yang.sh
@@ -14,6 +14,7 @@ ROUSETTE_YANG="${ROUSETTE_YANG:-/usr/share/yang/modules/rousette}"
 CFG_FS_YANG="${CFG_FS_YANG:-/usr/share/yang/modules/czechlight-cfg-fs}"
 PROC_CMDLINE="${PROC_CMDLINE:-/proc/cmdline}"
 CFG_SYSREPO_DIR="${CFG_SYSREPO_DIR:-/cfg/sysrepo}"
+TEST_RUN_SKIP_CHOWN="${TEST_RUN_SKIP_CHOWN:-0}"
 
 source ${SYSREPO_SETUP_DIR}/yang.sh
 source ${NETOPEER2_SETUP_DIR}/yang.sh
@@ -77,6 +78,7 @@ fi
 
 ALARM_MODULES=(
     "--install ${ALARMS_YANG}/ietf-alarms@2019-09-11.yang"
+        "--permissions 0644"
         "--enable-feature alarm-history"
         "--enable-feature alarm-shelving"
         "--enable-feature alarm-summary"
@@ -115,6 +117,7 @@ CFG_FS_MODULES=(
 CLA_MODULES=(
     "--install ${CLA_YANG}/iana-hardware@2018-03-13.yang"
     "--install ${CLA_YANG}/ietf-hardware@2018-03-13.yang"
+        "--permissions 0644"
         "--enable-feature hardware-sensor"
         "--enable-feature hardware-state"
 )
@@ -129,6 +132,14 @@ for ARG in $(cat "$PROC_CMDLINE"); do
     esac
 done
 
+if [[ "${TEST_RUN_SKIP_CHOWN}" == "1" ]]; then
+    echo "Not adding '--group optics' because this is a test run"
+    CLA_OPTICS_OPTS=""
+else
+    CLA_OPTICS_OPTS="--group optics"
+fi
+CLA_OPTICS_OPTS="${CLA_OPTICS_OPTS} --permissions 0664"
+
 case "${CZECHLIGHT}" in
     "")
         # no device model set -> do nothing
@@ -136,7 +147,9 @@ case "${CZECHLIGHT}" in
     sdn-roadm-line-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-roadm-common@2021-03-05.yang"
+                "${CLA_OPTICS_OPTS}"
             "--install ${CLA_YANG}/czechlight-roadm-device@2025-11-24.yang"
+                "${CLA_OPTICS_OPTS}"
                 "--enable-feature hw-line-9"
         )
         VELIA_MODULES+=(
@@ -146,7 +159,9 @@ case "${CZECHLIGHT}" in
     sdn-roadm-add-drop-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-roadm-common@2021-03-05.yang"
+                "${CLA_OPTICS_OPTS}"
             "--install ${CLA_YANG}/czechlight-roadm-device@2025-11-24.yang"
+                "${CLA_OPTICS_OPTS}"
                 "--enable-feature hw-add-drop-20"
         )
         VELIA_MODULES+=(
@@ -156,7 +171,9 @@ case "${CZECHLIGHT}" in
     sdn-roadm-hires-add-drop-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-roadm-common@2021-03-05.yang"
+                "${CLA_OPTICS_OPTS}"
             "--install ${CLA_YANG}/czechlight-roadm-device@2025-11-24.yang"
+                "${CLA_OPTICS_OPTS}"
                 "--enable-feature hw-add-drop-20"
                 "--enable-feature pre-wss-ocm"
         )
@@ -167,6 +184,7 @@ case "${CZECHLIGHT}" in
     sdn-roadm-coherent-a-d-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-coherent-add-drop@2025-11-24.yang"
+                "${CLA_OPTICS_OPTS}"
         )
         VELIA_MODULES+=(
             "--install ${VELIA_YANG}/czechlight-network-sdn-generic@2025-06-06.yang"
@@ -175,6 +193,7 @@ case "${CZECHLIGHT}" in
     sdn-inline-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-inline-amp@2021-03-05.yang"
+                "${CLA_OPTICS_OPTS}"
         )
         VELIA_MODULES+=(
             "--install ${VELIA_YANG}/czechlight-network-sdn-inline@2025-06-06.yang"
@@ -183,11 +202,13 @@ case "${CZECHLIGHT}" in
     calibration-box)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-calibration-device@2019-06-25.yang"
+                "${CLA_OPTICS_OPTS}"
         )
         ;;
     sdn-bidi-cplus1572-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-bidi-amp@2025-05-22.yang"
+                "${CLA_OPTICS_OPTS}"
                 "--enable-feature dualband-c-plus-1572"
         )
         VELIA_MODULES+=(
@@ -197,6 +218,7 @@ case "${CZECHLIGHT}" in
     sdn-bidi-cplus1572-ocm-g2)
         CLA_MODULES+=(
             "--install ${CLA_YANG}/czechlight-bidi-amp@2025-05-22.yang"
+                "${CLA_OPTICS_OPTS}"
                 "--enable-feature dualband-c-plus-1572"
                 "--enable-feature c-band-ocm"
         )

package/czechlight-cfg-fs/czechlight-cfg-fs.mk
@@ -135,6 +135,7 @@ czechlight-cfg-fs-test-migrations: $(BUILD_DIR)/czechlight-cfg-fs/.stamp_configu
 		ROUSETTE_SRCDIR=$(ROUSETTE_SRCDIR) \
 		NETOPEER2_TARGET_DIR=$(if $(filter y,$(BR2_PER_PACKAGE_DIRECTORIES)),$(PER_PACKAGE_DIR)/$(NETOPEER2_NAME)/target,$(BASE_TARGET_DIR)) \
 		PYTHONDONTWRITEBYTECODE=1 \
+		TEST_RUN_SKIP_CHOWN=1 \
 		pytest \
 			-vv \
 			--basetemp $(BUILD_DIR)/czechlight-cfg-fs/pytest \