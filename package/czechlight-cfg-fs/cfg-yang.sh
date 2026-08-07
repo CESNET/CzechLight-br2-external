CESNET/CzechLight-br2-external

commit e143c78b6e453d7fdcd877a15b8a1193e193f64f

Sync the YANG stack
This adds support for the "private candidate" DS. The Buildroot
packaging is now in the `cesnet/2026-06-10` branch.

The YANG setup got even more complex today. Previously, some extra
module installation was only needed for netopeer2, but not it's also
required for sysrepo -- hence the Buildroot update. Unfortunately, both
sysrepo and netopeer2 now install the `ietf-subscribed-notifications`
module, but with a different set of YANG-level features. Because we're
trying really, really hard to build a *single* installation command with
all the required YANG modules, we now have to monkey-patch this crap at
two places. Yay.

So, let's treat sysrepo's modules the same way as netopeer2's modules,
do everything at the first instance (with sysrepo, splicing in all the
required features for both netopeer2 and rousette, and be done with it,
right? Nope, of course, because sysrepo decided to split its in-the-tree
YANG modules into two directories for increased fun. All these modules
are installed into a single systemwide location, but that means that we
have to change some env variables to make this day even more
interesting. Fortunately, at least netopeer2 depends on both sysrpeo and
libnetconf2, so we can get away with reusing a single variable for this.
Yay again.

Change-Id: I7199c57e32bd5bf67513c82bbd026081919e35f0
Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/9224
Depends-on: https://gerrit.cesnet.cz/c/CzechLight/rousette/+/9228
Depends-on: https://gerrit.cesnet.cz/c/CzechLight/rousette/+/8716

package/czechlight-cfg-fs/cfg-yang.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
+SYSREPO_MODULE_DIR="${SYSREPO_MODULE_DIR:-/usr/share/yang/modules/sysrepo}"
 LN2_MODULE_DIR="${LN2_MODULE_DIR:-/usr/share/yang/modules/libnetconf2}"
 NP2_MODULE_DIR="${NP2_MODULE_DIR:-/usr/share/yang/modules/netopeer2}"
+SYSREPO_SETUP_DIR="${SYSREPO_SETUP_DIR:-/usr/libexec/sysrepo}"
 NETOPEER2_SETUP_DIR="${NETOPEER2_SETUP_DIR:-/usr/libexec/netopeer2}"
 CLA_YANG="${CLA_YANG:-/usr/share/yang/modules/cla-sysrepo}"
 VELIA_YANG="${VELIA_YANG:-/usr/share/yang/modules/velia}"
@@ -13,6 +15,7 @@ CFG_FS_YANG="${CFG_FS_YANG:-/usr/share/yang/modules/czechlight-cfg-fs}"
 PROC_CMDLINE="${PROC_CMDLINE:-/proc/cmdline}"
 CFG_SYSREPO_DIR="${CFG_SYSREPO_DIR:-/cfg/sysrepo}"
 
+source ${SYSREPO_SETUP_DIR}/yang.sh
 source ${NETOPEER2_SETUP_DIR}/yang.sh
 
 ROUSETTE_MODULES=(
@@ -23,23 +26,52 @@ ROUSETTE_MODULES=(
     "--install ${ROUSETTE_YANG}/rousette@2026-04-20.yang"
 )
 
-# The "ietf-subscribed-notifications" YANG module is already installed by netopeer2, but without
-# the "encode-json" feature enabled, so we have to monkey-patch that in.
-NETOPEER2_YANG_SETUP_COUNT=${#NETOPEER2_YANG_SETUP[@]}
+# The "ietf-subscribed-notifications" YANG module is already installed by both
+# sysrepo and netopeer2, each time with a different set of features.
+# We have to monkey-patch that.
+SYSREPO_YANG_SETUP_COUNT=${#SYSREPO_YANG_SETUP[@]}
 IETF_SUBSCIBED_NOTIFICATIONS_JSON=0
-for (( i=1; i<$NETOPEER2_YANG_SETUP_COUNT; i++ )); do
-    if [[ ${NETOPEER2_YANG_SETUP[i]} =~ "/ietf-subscribed-notifications@" ]]; then
-        NETOPEER2_YANG_SETUP=(
-            "${NETOPEER2_YANG_SETUP[@]:0:i+1}"
+for (( i=0; i<$SYSREPO_YANG_SETUP_COUNT; i++ )); do
+    if [[ ${SYSREPO_YANG_SETUP[i]} =~ "/ietf-subscribed-notifications@" ]]; then
+        SYSREPO_YANG_SETUP=(
+            "${SYSREPO_YANG_SETUP[@]:0:i+1}"
+            # this is needed for netopeer2, and we're building a single installation command,
+            # so let's prevent an error later on due to duplicate modules
+            "--enable-feature encode-xml"
+            # this one is for rousette
             "--enable-feature encode-json"
-            "${NETOPEER2_YANG_SETUP[@]:i+1}"
+            "${SYSREPO_YANG_SETUP[@]:i+1}"
         )
         IETF_SUBSCIBED_NOTIFICATIONS_JSON=1
         break
     fi
 done
-if [[ ${IETF_SUBSCIBED_NOTIFICATIONS_JSON} == 0 ]]; then
-    echo "YANG script error: cannot enable 'encode-json' for ietf-subscribed-notifications"
+if (( !IETF_SUBSCIBED_NOTIFICATIONS_JSON )); then
+    echo "YANG script error: cannot enable 'encode-json' and 'encode-xml' for 'ietf-subscribed-notifications'"
+    exit 1
+fi
+
+NETOPEER2_YANG_SETUP_MODIFIED=()
+NP2_FILTER_SKIP=0
+NP2_REMOVED_SUBSCRIBED_NOTIFICATIONS=0
+for LINE in "${NETOPEER2_YANG_SETUP[@]}"; do
+    echo "LINE: |${LINE}|"
+    if (( NP2_FILTER_SKIP )) && [[ ${LINE} =~ ^[[:space:]]?-i[[:space:]] || ${LINE} =~ ^[[:space:]]?--install[[:space:]] ]]; then
+        NP2_FILTER_SKIP=0
+        NP2_REMOVED_SUBSCRIBED_NOTIFICATIONS=1
+    fi
+
+    if [[ "$LINE" =~ "/ietf-subscribed-notifications@" ]]; then
+        NP2_FILTER_SKIP=1
+        continue
+    fi
+
+    if (( !NP2_FILTER_SKIP )); then
+        NETOPEER2_YANG_SETUP_MODIFIED+=("${LINE}")
+    fi
+done
+if (( ! NP2_REMOVED_SUBSCRIBED_NOTIFICATIONS )); then
+    echo "YANG script error: cannot filter out duplicate 'ietf-subscribed-notifications' from netopeer2 setup"
     exit 1
 fi
 
@@ -181,7 +213,8 @@ esac
 sysrepoctl \
     -v2 \
     --search-dirs ${NP2_MODULE_DIR}:${CLA_YANG}:${VELIA_YANG}:${ALARMS_YANG}:${ROUSETTE_YANG} \
-    ${NETOPEER2_YANG_SETUP[@]} \
+    ${SYSREPO_YANG_SETUP[@]} \
+    ${NETOPEER2_YANG_SETUP_MODIFIED[@]} \
     ${ROUSETTE_MODULES[@]} \
     ${ALARM_MODULES[@]} \
     ${VELIA_MODULES[@]} \

package/czechlight-cfg-fs/czechlight-cfg-fs.mk
@@ -80,6 +80,7 @@ define CZECHLIGHT_CFG_FS_INSTALL_TARGET_CMDS
 		sysrepo-ietf-alarms.service \
 		sysrepo-persistent-cfg.service \
 		sysrepo-plugind.service \
+		sysrepo-notifd.service \
 		velia-firewall.service \
 		velia-health.service \
 		velia-system.service \
@@ -132,8 +133,7 @@ czechlight-cfg-fs-test-migrations: $(BUILD_DIR)/czechlight-cfg-fs/.stamp_configu
 		VELIA_SRCDIR=$(VELIA_SRCDIR) \
 		SYSREPO_IETF_ALARMS_SRCDIR=$(SYSREPO_IETF_ALARMS_SRCDIR) \
 		ROUSETTE_SRCDIR=$(ROUSETTE_SRCDIR) \
-		LIBNETCONF2_SRCDIR=$(LIBNETCONF2_SRCDIR) \
-		NETOPEER2_SRCDIR=$(NETOPEER2_SRCDIR) \
+		NETOPEER2_TARGET_DIR=$(if $(filter y,$(BR2_PER_PACKAGE_DIRECTORIES)),$(PER_PACKAGE_DIR)/$(NETOPEER2_NAME)/target,$(BASE_TARGET_DIR)) \
 		PYTHONDONTWRITEBYTECODE=1 \
 		pytest \
 			-vv \

tests/czechlight-cfg-fs/migrations.py
@@ -48,8 +48,10 @@ class SysrepoFixture:
         res = os.environ.copy()
         res['SYSREPO_SHM_PREFIX'] = self._shm_prefix
         res['SYSREPO_REPOSITORY_PATH'] = self._repo_path
-        res['LN2_MODULE_DIR'] = pathlib.Path(os.environ['LIBNETCONF2_SRCDIR']) / 'modules'
-        res['NP2_MODULE_DIR'] = pathlib.Path(os.environ['NETOPEER2_SRCDIR']) / 'modules'
+        res['SYSREPO_MODULE_DIR'] = pathlib.Path(os.environ['NETOPEER2_TARGET_DIR']) / 'usr' / 'share' / 'yang' / 'modules' / 'sysrepo'
+        res['LN2_MODULE_DIR'] = pathlib.Path(os.environ['NETOPEER2_TARGET_DIR']) / 'usr' / 'share' / 'yang' / 'modules' / 'libnetconf2'
+        res['NP2_MODULE_DIR'] = pathlib.Path(os.environ['NETOPEER2_TARGET_DIR']) / 'usr' / 'share' / 'yang' / 'modules' / 'netopeer2'
+        res['SYSREPO_SETUP_DIR'] = BR2_EXT_ROOT / 'submodules' / 'buildroot' / 'package' / 'sysrepo'
         res['NETOPEER2_SETUP_DIR'] = BR2_EXT_ROOT / 'submodules' / 'buildroot' / 'package' / 'netopeer2'
         res['CLA_YANG'] = pathlib.Path(os.environ['CLA_SYSREPO_SRCDIR']) / 'yang'
         res['VELIA_YANG'] = pathlib.Path(os.environ['VELIA_SRCDIR']) / 'yang'

commit 5b29cf98603802ccc9da94953fa7814579901e08

scripts: fix a typo
Fixes: e143c78b Sync the YANG stack
Change-Id: I0c3b1e2458cf2ce8821b3de10275dc387cd96ed0

package/czechlight-cfg-fs/cfg-yang.sh
@@ -30,7 +30,7 @@ ROUSETTE_MODULES=(
 # sysrepo and netopeer2, each time with a different set of features.
 # We have to monkey-patch that.
 SYSREPO_YANG_SETUP_COUNT=${#SYSREPO_YANG_SETUP[@]}
-IETF_SUBSCIBED_NOTIFICATIONS_JSON=0
+IETF_SUBSCRIBED_NOTIFICATIONS_JSON=0
 for (( i=0; i<$SYSREPO_YANG_SETUP_COUNT; i++ )); do
     if [[ ${SYSREPO_YANG_SETUP[i]} =~ "/ietf-subscribed-notifications@" ]]; then
         SYSREPO_YANG_SETUP=(
@@ -42,11 +42,11 @@ for (( i=0; i<$SYSREPO_YANG_SETUP_COUNT; i++ )); do
             "--enable-feature encode-json"
             "${SYSREPO_YANG_SETUP[@]:i+1}"
         )
-        IETF_SUBSCIBED_NOTIFICATIONS_JSON=1
+        IETF_SUBSCRIBED_NOTIFICATIONS_JSON=1
         break
     fi
 done
-if (( !IETF_SUBSCIBED_NOTIFICATIONS_JSON )); then
+if (( !IETF_SUBSCRIBED_NOTIFICATIONS_JSON )); then
     echo "YANG script error: cannot enable 'encode-json' and 'encode-xml' for 'ietf-subscribed-notifications'"
     exit 1
 fi